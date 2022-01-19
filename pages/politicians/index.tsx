21035d7c1c44623502a7ba88b93fecd4ee949f0e
Wiley Conte <wileymckayconte@gmail.com>
2022-01-19 13:36:04 -0700

    Add Head component to politician index

--- pages/politicians/index.tsx
+++ pages/politicians/index.tsx
@@ -11,6 +11,7 @@ import useDeviceInfo from "../../hooks/useDeviceInfo";
 import Spacer from "../../components/Spacer/Spacer";
 import { useState } from "react";
 import { dehydrate, QueryClient } from "react-query";
+import Head from "next/head";
 
 const PoliticianRow = ({
   politician,
@@ -122,88 +123,99 @@ const PoliticianIndex: NextPage = () => {
   };
 
   return (
-    <Layout>
-      <div className={`${styles.stickyMainHeader} ${styles.shadow}`}>
-        {!isMobile && <h1>Colorado Legislators</h1>}
+    <>
+      <Head>
+        <title>Populist - Politician Browser</title>
+        <meta
+          name="description"
+          content="Find information on your government representatives like voting histories, endorsements, and financial data."
+        />
+      </Head>
+      <Layout>
+        <div className={`${styles.stickyMainHeader} ${styles.shadow}`}>
+          {!isMobile && <h1>Colorado Legislators</h1>}
 
-        {/* <div className={styles.mb3}>
+          {/* <div className={styles.mb3}>
           <p>Find your legislators by entering your address.</p>
           <input placeholder="Enter address"></input>
           <button>Search</button>
         </div> */}
-        <div className={styles.filtersContainer}>
-          {!isMobile && <h2>Browse</h2>}
-          <div className={styles.inputWithIcon}>
-            <input
-              placeholder="Search"
-              onChange={(e) => setSearchQuery(e.target.value)}
-            ></input>
-            <FaSearch color="var(--blue)" />
+          <div className={styles.filtersContainer}>
+            {!isMobile && <h2>Browse</h2>}
+            <div className={styles.inputWithIcon}>
+              <input
+                placeholder="Search"
+                onChange={(e) => setSearchQuery(e.target.value)}
+              ></input>
+              <FaSearch color="var(--blue)" />
+            </div>
+            <Spacer size={16} axis="vertical" />
+            <form className={`${styles.flexBetween}`}>
+              <input
+                name="scope"
+                id="federal-radio"
+                type="radio"
+                value="federal"
+                checked={localityFilter === "federal"}
+                onChange={(e) =>
+                  setLocalityFilter(e.target.value as LocalityFilter)
+                }
+              />
+              <label htmlFor="federal-radio" className={styles.radioLabel}>
+                Federal
+              </label>
+              <input
+                name="scope"
+                id="state-radio"
+                type="radio"
+                value="state"
+                checked={localityFilter === "state"}
+                onChange={(e) =>
+                  setLocalityFilter(e.target.value as LocalityFilter)
+                }
+              />
+              <label htmlFor="state-radio" className={styles.radioLabel}>
+                State
+              </label>
+              <select
+                name="chamber"
+                onChange={(e) =>
+                  setChamberFilter(e.target.value as ChamberFilter)
+                }
+              >
+                <option value="all">All Chambers</option>
+                <option value="house">House</option>
+                <option value="senate">Senate</option>
+              </select>
+            </form>
           </div>
-          <Spacer size={16} axis="vertical" />
-          <form className={`${styles.flexBetween}`}>
-            <input
-              name="scope"
-              id="federal-radio"
-              type="radio"
-              value="federal"
-              checked={localityFilter === "federal"}
-              onChange={(e) =>
-                setLocalityFilter(e.target.value as LocalityFilter)
-              }
-            />
-            <label htmlFor="federal-radio" className={styles.radioLabel}>
-              Federal
-            </label>
-            <input
-              name="scope"
-              id="state-radio"
-              type="radio"
-              value="state"
-              checked={localityFilter === "state"}
-              onChange={(e) =>
-                setLocalityFilter(e.target.value as LocalityFilter)
-              }
-            />
-            <label htmlFor="state-radio" className={styles.radioLabel}>
-              State
-            </label>
-            <select
-              name="chamber"
-              onChange={(e) =>
-                setChamberFilter(e.target.value as ChamberFilter)
-              }
-            >
-              <option value="all">All Chambers</option>
-              <option value="house">House</option>
-              <option value="senate">Senate</option>
-            </select>
-          </form>
         </div>
-      </div>
-      <div>
-        {isLoading && <LoaderFlag />}
-        {error && <h4>Something went wrong fetching politician records...</h4>}
         <div>
-          {data &&
-            data.allPoliticians
-              .filter((p) =>
-                localityFilterFn(p as PoliticianResult, localityFilter)
-              )
-              .filter((p) =>
-                chamberFilterFn(p as PoliticianResult, chamberFilter)
-              )
-              .filter((p) =>
-                p.fullName
-                  .toLowerCase()
-                  .includes(searchQuery?.toLowerCase() || "")
-              )
-              .map((politician) => (
-                <PoliticianRow politician={politician} key={politician.id} />
-              ))}
+          {isLoading && <LoaderFlag />}
+          {error && (
+            <h4>Something went wrong fetching politician records...</h4>
+          )}
+          <div>
+            {data &&
+              data.allPoliticians
+                .filter((p) =>
+                  localityFilterFn(p as PoliticianResult, localityFilter)
+                )
+                .filter((p) =>
+                  chamberFilterFn(p as PoliticianResult, chamberFilter)
+                )
+                .filter((p) =>
+                  p.fullName
+                    .toLowerCase()
+                    .includes(searchQuery?.toLowerCase() || "")
+                )
+                .map((politician) => (
+                  <PoliticianRow politician={politician} key={politician.id} />
+                ))}
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
