9cca8b6e2546fe8169b97c559c9d0893acc4b25b
Wiley Conte <wileymckayconte@gmail.com>
2022-03-09 16:29:50 -0700

    Add react hook form

--- components/SignUp/SignUp.tsx
+++ components/SignUp/SignUp.tsx
@@ -1,7 +1,14 @@
 import { LogoText } from "components/LogoText/LogoText";
+import { useForm } from "react-hook-form";
 import styles from "./SignUp.module.scss";
 
 export function SignUp() {
+  const { register, handleSubmit, formState } = useForm();
+
+  console.log(formState.errors);
+
+  const submitForm = (data: any) => alert(JSON.stringify(data));
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -11,9 +18,24 @@ export function SignUp() {
       </div>
       <div className={styles.formWrapper}>
         <h1 className="title">Get Started</h1>
-        <p>All we need is your email to get started.</p>
-        <form>
-          <input type="text" name="email" placeholder="Email" />
+        <p>All we need is your email to get started. </p>
+        <form onSubmit={handleSubmit(submitForm)}>
+          <div className={styles.inputWrapper}>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            <small className={styles.inputError}>
+              {formState.errors.email && formState.errors.email.message}
+            </small>
+          </div>
           <button>Create Account</button>
         </form>
       </div>
