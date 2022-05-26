124fe8c529e81c005d1fa4bb126938beaed0ce33
Zack <zackrose@gmail.com>
2022-05-25 17:56:17 -0700

    Update voting guide page to be dynamic

--- pages/voting-guides/index.tsx
+++ pages/voting-guides/index.tsx
@@ -1,33 +1,93 @@
 import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useUpsertVotingGuideMutation, useVotingGuidesByUserIdQuery, VotingGuideResult } from "generated";
+import { 
+  Scalars,
+  useUpsertVotingGuideMutation,
+  useVotingGuidesByUserIdQuery,
+  VotingGuideResult } from "generated";
 import { Layout, Avatar, FlagSection, Button } from "components";
 import { useAuth } from "hooks/useAuth";
 import styles from "./VotingGuides.module.scss";
 
-interface Props {
-  bla?: string
+
+const VotingGuideCard = (/*{ guide }:{ guide: VotingGuideResult }*/) => {
+  return(
+  <div className={styles.roundedCard}>
+  <div className={styles.guideContainer}>
+    <div className={styles.avatarContainer}>
+      <Avatar src="https://www.gravatar.com/avatar/" size={80} fallbackSrc="https://www.gravatar.com/avatar/" alt="[user name here]" />
+      <h4>[User name here]</h4>
+    </div>
+    <div className={styles.buttonWrapper}>
+      <Button large primary theme="blue" label="Edit">Edit</Button>
+      <Button large primary theme="yellow" label="Share">Share</Button>
+    </div>
+  </div>
+</div>);
+};
+
+interface VotingGuideElection {
+  date: string,
+  description?: string,
+  guides: VotingGuideResult[],  
+  id: Scalars['ID'],
+  title: string,
 }
 
-// const formatRaceDate(date:String)
+const getElectionsFromGuides = (guides: VotingGuideResult[]): VotingGuideElection[]=>{
+  if (guides){
+    return guides.reduce<VotingGuideElection[]>((result, value) => {
+      const i = result.findIndex(v=>v.id===value.electionId);
+      if (i<0) {
+        const electionInfo = {
+          id: value.electionId,
+          title: value.election.title,
+          description: value.election.description,
+          date: value.election.electionDate,
+          guides: [value]
+        } as VotingGuideElection;
+        result.push(electionInfo);
+      } else {
+        result[i]?.guides.push(value);
+      }
+      return result;
+    }, [] as VotingGuideElection[]);
+  } else return [] as VotingGuideElection[];
+};
 
-const VotingGuides: NextPage<Props> = () => {
+const VotingGuides: NextPage<{ mobileNavTitle?: string }> = ({mobileNavTitle}) => {
 
-  const user = useAuth({}).user;
-  const upsertVotingGuide = useUpsertVotingGuideMutation();
-  const votingGuidesByUserId = useVotingGuidesByUserIdQuery({userId:user?.id || ""});
-  const [guides, setGuides] = useState<VotingGuideResult[]>([]);
+  const user = useAuth({redirectTo:"/login"}).user; //user will not be null under this line, due to the conditional redirect
+  const votingGuidesByUserId = useVotingGuidesByUserIdQuery({userId:user?.id || ""}); 
+  const [elections, setElections] = useState<VotingGuideElection[]>([]);
 
   useEffect(()=>{
-    const {data} = votingGuidesByUserId;
-    if (user && data?.votingGuidesByUserId) {
+    const {data, isError, error} = votingGuidesByUserId;
+  if (isError){
+    alert("Error getting Voting Guides");
+    console.error("Error getting Voting Guides", error);
+  }
+  else{
+    if (data && data.votingGuidesByUserId.length) {
       const newVotingGuides = data.votingGuidesByUserId as VotingGuideResult[];
-      setGuides(newVotingGuides);
+      const elections = getElectionsFromGuides(newVotingGuides);
+      console.log('yes data', elections);
+      setElections(elections);
+    } else {
+      votingGuidesByUserId.refetch().then(res=>{
+        const elections = getElectionsFromGuides(res.data?.votingGuidesByUserId as VotingGuideResult[]);
+        console.log('no data', elections);
+        setElections(elections);
+      });
     }
-  },[user, setGuides, votingGuidesByUserId]);
-  
+  };
+  },[]);
 
+  const upsertVotingGuide = useUpsertVotingGuideMutation();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   return (
     <>
@@ -36,39 +96,46 @@ const VotingGuides: NextPage<Props> = () => {
         <meta name="description" content="View Voting Guides." />
       </Head>
 
-      <Layout showNavLogoOnMobile={false}>
-        <FlagSection title="Voting Guides">
-        {
-          guides.map(guide=>{
-            <div></div>
-
-          })
-
-        }
-
-        <h1>June 28, 2022</h1>
-        <hr />
-        <h2>Colorado Primaries</h2>
-        <p className={styles.smallText}>Primary elections for the general election later this year on November 8, 2022.</p>
-        <div className={styles.roundedCard}>
-          <div className={styles.guideContainer}>
-            <div className={styles.avatarContainer}>
-              <Avatar src="https://www.gravatar.com/avatar/" size={80} fallbackSrc="https://www.gravatar.com/avatar/" alt="Henry Lai" />
-              <h4>Henry Lai</h4>
-            </div>
-            <div className={styles.buttonWrapper}>
-              <Button large primary theme="blue" label="Edit">Edit</Button>
-              <Button large primary theme="yellow" label="Share">Share</Button>
-              <Button large theme="blue" label="Add guide" onClick={()=>{
-                upsertVotingGuide.mutate({
-                  title: "Zack's Guide",
-                  description: "Vote or die, this is why"});
-              }}>Share</Button>
-            </div>
-          </div>
+      <Layout mobileNavTitle={`${mobileNavTitle || 'Voting Guides'}`} showNavLogoOnMobile={false}>
+        <div className={styles.votingContainer}> 
+          <FlagSection title="Voting Guides">
+            {
+              elections.map(election=>(
+                <div key={election.id}>
+                  <h1 className={styles.votingHeader}>{election.date}</h1>
+                  <h2>{election.title}</h2>
+                  <p className={styles.description}>{election.description}</p>
+                    {
+                      election.guides?.map(guide=><VotingGuideCard key={guide.id} />)
+                    }
+                </div>
+              ))
+            }
+          </FlagSection>
         </div>
 
-        </FlagSection>
+        <div className={styles.roundedCard}>
+          <h2>Add a test guide </h2>
+          <h3>(for CO Primary Election)</h3>
+          <input className={styles.guideInput} type="text" placeholder="Title" onChange={e=>setTitle(e.target.value)} value={title} />
+          <input className={styles.guideInput} type="text" placeholder="Description" onChange={e=>setDescription(e.target.value)} value={description} />
+          <Button large theme="blue" disabled={!title || !description} label="add_guide"
+            onClick={()=>
+              upsertVotingGuide.mutate({
+                electionId:"652b6615-e822-4cc1-9959-6d4c15d0acea",
+                title,
+                description
+              },
+              {
+                onSuccess: ()=>alert("Your guide was added"),
+                onError: (err)=>{
+                  alert("Your guide was not added");
+                  console.error("guide not added", err);
+                }
+              }
+            )
+          }>Add Test Guide</Button>
+        </div>
       </Layout>
     </>
   );
