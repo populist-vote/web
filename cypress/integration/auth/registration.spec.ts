715b42f6e7a2eb07bb68149abd1084ebfe502f32
Zack <zackrose@gmail.com>
2022-05-26 16:38:30 -0700

    reset prettier autoformat on files I didn't touch

--- cypress/integration/auth/registration.spec.ts
+++ cypress/integration/auth/registration.spec.ts
@@ -4,16 +4,6 @@ describe("the registration page", () => {
   });
 
   it("should display the first step of registration form", () => {
-    cy.get('[data-testid="login-form"]').should("be.visible");
+    cy.get('[data-testid="register-form-1"]').should("be.visible");
   });
-
-  it("should validate the first step", () => {});
-
-  it("should take the user to the address step if the email does not exist", () => {});
-  
-  it("should allow the user to register when the form is complete and take the user to the ballot page", () => {
-    cy.get('[data-testid="register-form]').should("be.visible");
-  });
-
-  
 });
