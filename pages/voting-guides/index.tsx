321b5542d60bec060426d913808f6e77a5eb5fe8
Wiley Conte <wileymckayconte@gmail.com>
2022-05-29 12:00:43 -0600

    Update styles for guide items

--- pages/voting-guides/index.tsx
+++ pages/voting-guides/index.tsx
@@ -10,6 +10,7 @@ import { Layout, Avatar, FlagSection, Button, LoaderFlag } from "components";
 import styles from "./VotingGuides.module.scss";
 import { useQueryClient } from "react-query";
 import { useAuth } from "hooks/useAuth";
+import { dateString } from "utils/dates";
 
 const VotingGuideCard = ({ guide }: { guide: Partial<VotingGuideResult> }) => {
   const { user } = guide;
@@ -18,27 +19,20 @@ const VotingGuideCard = ({ guide }: { guide: Partial<VotingGuideResult> }) => {
     ? `${firstName} ${!!lastName ? lastName : ""}`
     : username;
   return (
-    <div className={`${styles.roundedCard} ${styles.votingCardContainer}`}>
-      <div className={styles.guideContainer}>
-        <div className={styles.avatarContainer}>
-          <Avatar
-            src={avatarUrl || ""}
-            size={80}
-            fallbackSrc="https://www.gravatar.com/avatar/"
-            alt={name as string}
-          />
-          <h4>{name}</h4>
-        </div>
-        <div className={styles.buttonWrapper}>
-          <Button large primary theme="blue" label="Edit">
-            Edit
-          </Button>
-          <span className={styles.shareButton}>
-            <Button large primary theme="yellow" label="Share">
-              Share
-            </Button>
-          </span>
-        </div>
+    <div className={styles.guideContainer}>
+      <div className={styles.avatarContainer}>
+        <Avatar
+          src={avatarUrl || ""}
+          size={80}
+          fallbackSrc="https://www.gravatar.com/avatar/"
+          alt={name as string}
+        />
+        <h4>{name}</h4>
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Button large primary theme="yellow" label="Share">
+          Share
+        </Button>
       </div>
     </div>
   );
@@ -66,9 +60,6 @@ const VotingGuides: NextPage<{
 
   const election = data?.votingGuidesByUserId[0]?.election;
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-
   return (
     <>
       <Head>
@@ -82,8 +73,9 @@ const VotingGuides: NextPage<{
       >
         <div className={styles.votingContainer}>
           <FlagSection title="Voting Guides">
-            <div>
-              <h1>{election?.title}</h1>
+            <div className={styles.votingHeader}>
+              <h1>{dateString(election?.electionDate)}</h1>
+              <h2>{election?.title}</h2>
               <p>{election?.description}</p>
             </div>
             {isLoading && <LoaderFlag />}
@@ -95,48 +87,6 @@ const VotingGuides: NextPage<{
               />
             ))}
           </FlagSection>
-        </div>
-
-        <div className={styles.roundedCard}>
-          <h2>Add a test guide </h2>
-          <h3>(for CO Primary Election)</h3>
-          <input
-            className={styles.guideInput}
-            type="text"
-            placeholder="Title"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-          <input
-            className={styles.guideInput}
-            type="text"
-            placeholder="Description"
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-          />
-          <Button
-            large
-            theme="blue"
-            disabled={!title || !description}
-            label="add_guide"
-            onClick={() =>
-              upsertVotingGuide.mutate(
-                {
-                  electionId: "652b6615-e822-4cc1-9959-6d4c15d0acea",
-                  title,
-                  description,
-                },
-                {
-                  onError: (err) => {
-                    alert("Your guide was not added");
-                    console.error("guide not added", err);
-                  },
-                }
-              )
-            }
-          >
-            Add Test Guide
-          </Button>
         </div>
       </Layout>
     </>
