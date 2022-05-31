257838ecd008bdc67f7adb400a67d0d95c737894
Zack <zackrose@gmail.com>
2022-05-30 17:59:53 -0700

    Add border logic to Avatar

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,6 +1,7 @@
 import { ImageWithFallback, Add, Note } from "components";
 import React, { useMemo, CSSProperties } from "react";
 import { /*FaPlus, FaPlusCircle,*/ FaStar } from "react-icons/fa";
+import { default as classNames } from "classnames";
 import {
   ORGANIZATION_FALLBACK_IMAGE_URL,
   PERSON_FALLBACK_IMAGE_URL,
@@ -15,8 +16,10 @@ interface BadgeProps {
   fontSize: string;
 }
 
+type IconTypes = "plus" | "note" | "star";
+
 interface IconProps {
-  type: "plus" | "note" | "star";
+  type: IconTypes;
   background?: string;
   onClick?: () => void;
   size: string;
@@ -29,6 +32,7 @@ export interface AvatarProps {
   alt: string;
   size: number;
   badge?: BadgeProps;
+  borderColor?: string;
   icon?: IconProps;
   name?: string;
   round?: boolean;
@@ -40,6 +44,10 @@ export interface PartyAvatarProps extends AvatarProps {
   badgeSize?: string;
   badgeFontSize?: string;
   href?: string;
+  isEndorsed?: boolean;
+  iconSize?: string;
+  iconFontSize?: string;
+  iconType?: IconTypes;
 }
 
 function Badge(props: BadgeProps): JSX.Element {
@@ -90,13 +98,22 @@ function Icon(props: IconProps): JSX.Element {
 
   return (
     <div className={styles.iconWrapper} style={styleVars}>
-      <span className={styles.iconInner}>{$icon}</span>
+      <span className={styles.iconInner}>
+        <$icon />
+      </span>
     </div>
   );
 }
 
 function Avatar(props: AvatarProps): JSX.Element {
-  const { badge, icon } = props;
+  const { badge, icon, borderColor } = props;
+
+  const styleVars: CSSProperties & { "--bcol": string } = {
+    [`--bcol`]: borderColor || "",
+    borderColor,
+    borderWidth: borderColor ?? "2px",
+    borderStyle: borderColor ? "solid" : "none",
+  };
 
   return (
     <div style={{ display: "inline" }}>
@@ -106,7 +123,10 @@ function Avatar(props: AvatarProps): JSX.Element {
           fallbackSrc={props.fallbackSrc as string}
           width={props.size}
           height={props.size}
-          className={styles.imageContainer}
+          style={styleVars}
+          className={classNames(styles.imageContainer, {
+            [styles.border as string]: !!borderColor,
+          })}
         />
         {icon && <Icon {...icon} />}
         {badge && <Badge {...badge} />}
@@ -119,8 +139,12 @@ function PartyAvatar({
   party,
   badgeSize = "1.25rem",
   badgeFontSize = "0.75rem",
+  iconSize = "1.25rem",
+  iconFontSize = "0.75rem",
+  iconType = "plus",
   src,
   fallbackSrc = PERSON_FALLBACK_IMAGE_URL,
+  isEndorsed = false,
   ...rest
 }: PartyAvatarProps): JSX.Element {
   const partyColor = useMemo(() => getPartyColor(party), [party]);
@@ -132,7 +156,23 @@ function PartyAvatar({
     fontSize: badgeFontSize,
   };
 
-  return <Avatar badge={badge} src={src} fallbackSrc={fallbackSrc} {...rest} />;
+  const icon = {
+    background: isEndorsed ? "var(--yellow)" : "var(--grey)",
+    type: iconType,
+    size: iconSize,
+    fontSize: iconFontSize,
+  };
+
+  return (
+    <Avatar
+      borderColor={isEndorsed ? "var(--yellow)" : undefined}
+      badge={badge}
+      icon={icon}
+      src={src}
+      fallbackSrc={fallbackSrc}
+      {...rest}
+    />
+  );
 }
 
 function OrganizationAvatar({
