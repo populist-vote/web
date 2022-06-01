cbfb0e99f6bfbbede597f5099dfa6e89989dcf15
Wiley Conte <wileymckayconte@gmail.com>
2022-06-01 11:37:35 -0600

    Remove weird header section

--- components/Politician/HeaderSection.tsx
+++ components/Politician/HeaderSection.tsx
@@ -1,4 +1,4 @@
-import { Button, PartyAvatar } from "components";
+import { PartyAvatar } from "components";
 import styles from "styles/page.module.scss";
 import headerStyles from "./HeaderSection.module.scss";
 import { default as classNames } from "classnames";
@@ -31,36 +31,6 @@ function HeaderSection({
       />
       {!isMobile && (
         <h1 className={headerStyles.fullName}>{politician?.fullName}</h1>
-      )}
-      {false ? (
-        <Button id="add-note-btn" label={"Add note"} primary large theme="blue">
-          Add note
-        </Button>
-      ) : (
-        <div className={headerStyles.note}>
-          <div className={headerStyles.header}>Voting Guide Note</div>
-          <div className={headerStyles.noteText}>
-            It is something I have been posting for a few weeks – I usually
-            adjust the statement to whatever the post is related to. I have been
-            getting a laugh out of doing it and other people have seemed to
-            enjoy it too – Anyway – since this is my post and I have been going
-            around making variations of this comment people are now showing up
-            here on my post with their own variations of the comment. I hope
-            that clarifies things…. but please do not let this extensive
-            clarification distract you from the fact that in 1998, The
-            Undertaker threw Mankind off Hell In A Cell, and plummeted 16 ft
-            through an announcer's table."
-          </div>
-          <Button
-            id="edit-note-btn"
-            label={"Edit note"}
-            secondary
-            large
-            theme="blue"
-          >
-            Edit note
-          </Button>
-        </div>
       )}
     </section>
   );
