20178b226416243d8c3a3ae3788bff4d2645ee0d
Wiley Conte <wileymckayconte@gmail.com>
2022-06-07 22:29:34 -0600

    Add generic to TextInput so that it accepts register

--- components/TextInput/TextInput.tsx
+++ components/TextInput/TextInput.tsx
@@ -1,26 +1,38 @@
 import classNames from "classnames";
 import styles from "./TextInput.module.scss";
+import { UseFormRegister } from "react-hook-form";
 
-function TextInput({
-  id, label, hideLabel, value, onChange, placeholder
-} : {
-  id?: string,
-  label: string,
-  hideLabel?: boolean,
-  value?: string,
-  onChange?: () => void,
-  placeholder?: string
-}) {
+type TextInputProps<TFormValues> = {
+  id?: string;
+  label: string;
+  hideLabel?: boolean;
+  value?: string;
+  onChange?: () => void;
+  placeholder?: string;
+  register?: UseFormRegister<TFormValues>;
+};
+
+function TextInput<TFormValues extends Record<string, unknown>>({
+  id,
+  label,
+  hideLabel,
+  value,
+  onChange,
+  placeholder,
+}: TextInputProps<TFormValues>) {
   const inputId = id || "input";
   const inputClasses = classNames(styles.inputContainer, {
-    [styles.hideLabel as string]: hideLabel
+    [styles.hideLabel as string]: hideLabel,
   });
   return (
     <div className={inputClasses}>
-      <label htmlFor={inputId}>
-        {label}
-      </label>
-      <input value={value || ""} id={inputId} onChange={onChange} placeholder={placeholder || ""} />
+      <label htmlFor={inputId}>{label}</label>
+      <input
+        value={value || ""}
+        id={inputId}
+        onChange={onChange}
+        placeholder={placeholder || ""}
+      />
     </div>
   );
 }
