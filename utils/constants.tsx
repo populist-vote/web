9eb1c278cd67775830f0cf31ef1a22eccf5e0083
Zack <zackrose@gmail.com>
2022-06-14 11:57:51 -0700

    Add constant for welcome message localstorage

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,8 +1,13 @@
+// Urls
+
 export const PERSON_FALLBACK_IMAGE_URL =
   "https://populist-platform.s3.us-east-2.amazonaws.com/web-assets/politician-thumbnails/default-person-160.jpg";
 
 export const ORGANIZATION_FALLBACK_IMAGE_URL =
   "https://populist-platform.s3.us-east-2.amazonaws.com/web-assets/organization-thumbnails/default-org-160.jpg";
+
+// Local Storage
+export const VOTING_GUIDE_WELCOME_VISIBLE = "voting-guide-welcome-visible";
 
 export const SHARED_GUIDES_LOCAL_STORAGE =
   "populist-shared-guides-local-storage";
