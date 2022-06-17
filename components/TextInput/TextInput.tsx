c5a526692d4300e6a28626eab27563578e2dfbb5
Wiley Conte <wileymckayconte@gmail.com>
2022-06-17 10:58:27 -0600

    Update TextInput to work better with react-hook-form

--- components/TextInput/TextInput.tsx
+++ components/TextInput/TextInput.tsx
@@ -1,49 +1,66 @@
 import classNames from "classnames";
 import styles from "./TextInput.module.scss";
-import { UseFormRegister } from "react-hook-form";
+import {
+  ChangeHandler,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
 
 type TextInputProps<TFormValues> = {
   id?: string;
-  errors?: boolean | string[] | string
+  name: Path<TFormValues>;
+  errors?: boolean | string[] | string;
   label: string;
   hideLabel?: boolean;
   value?: string;
-  onChange?: () => void;
+  onChange?: ChangeHandler | (() => void);
   placeholder?: string;
   register?: UseFormRegister<TFormValues>;
-  type?: "text" | "password"
+  rules?: RegisterOptions;
+  type?: "text" | "password";
 };
 
 function TextInput<TFormValues extends Record<string, unknown>>({
   id,
+  name,
   errors = false,
   label,
   hideLabel,
-  value,
-  onChange,
   placeholder,
-  type = "text"
+  type = "text",
+  register,
+  rules,
+  ...rest
 }: TextInputProps<TFormValues>) {
   const inputId = id || "input";
   const inputClasses = classNames(styles.inputContainer, {
     [styles.hideLabel as string]: hideLabel,
   });
-  const hasErrors = Array.isArray(errors) ? (
-    errors.length > 0
-  ) : (
-    typeof errors === "string" ? errors.length > 0 : !!errors
-  );
-  const errorMessage = errors?.join?.(" ") || errors || "";
+  const hasErrors = Array.isArray(errors)
+    ? errors.length > 0
+    : typeof errors === "string"
+    ? errors.length > 0
+    : !!errors;
+
+  const errorMessage = () => {
+    if (typeof errors !== "string" && typeof errors !== "boolean") {
+      return errors?.join?.(" ");
+    } else {
+      return errors || "";
+    }
+  };
+
   return (
     <div className={inputClasses}>
       <label htmlFor={inputId}>{label}</label>
       <input
-        aria-invalid={hasErrors}
-        type={type}
-        value={value || ""}
         id={inputId}
-        onChange={onChange}
+        type={type}
         placeholder={placeholder || ""}
+        aria-invalid={hasErrors}
+        {...rest}
+        {...(register && register(name, rules))}
       />
       {hasErrors && <span className={styles.errorMessage}>{errorMessage}</span>}
     </div>
