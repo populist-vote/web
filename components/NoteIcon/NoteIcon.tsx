bd70d0959bd1387a8879640320d6a7ddeac96343
Wiley Conte <wileymckayconte@gmail.com>
2022-06-23 02:41:19 -0600

    Remove unecessary return from  upsertVotingGuideCandidate

--- components/NoteIcon/NoteIcon.tsx
+++ components/NoteIcon/NoteIcon.tsx
@@ -2,13 +2,5 @@ import Image from "next/image";
 import NoteIconSVG from "public/images/NoteIcon.svg";
 
 export default function NoteIcon({ ...rest }) {
-  return (
-    <Image
-      src={NoteIconSVG}
-      alt="Note"
-      objectFit="contain"
-      priority
-      {...rest}
-    />
-  );
+  return <Image src={NoteIconSVG} alt="Note" priority {...rest} />;
 }
