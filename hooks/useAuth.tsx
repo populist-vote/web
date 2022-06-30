3984235556321920564ae7077fb2c3badd412c75
Wiley McKay Conte <30814801+wileymc@users.noreply.github.com>
2022-06-29 23:47:10 -0600

    Implement basic admin panel (#68)

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-import { AuthTokenResult, useCurrentUserQuery } from "../generated";
+import { AuthTokenResult, Role, useCurrentUserQuery } from "../generated";
 import Router from "next/router";
 import { createContext, ReactNode, useContext, useEffect } from "react";
 
@@ -7,7 +7,7 @@ const AuthContext = createContext<AuthTokenResult>({} as AuthTokenResult);
 export function AuthProvider({ children }: { children: ReactNode }) {
   const { data, isLoading, error } = useCurrentUserQuery();
   if (isLoading || error) return null;
-  
+
   return (
     <AuthContext.Provider value={data?.currentUser as AuthTokenResult}>
       {children}
@@ -15,12 +15,29 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   );
 }
 
-export function useAuth({ redirectTo = "/login" }: { redirectTo?: string } = {}) {
+export function useAuth({
+  redirectTo = "/login",
+  minRole = Role.Basic,
+}: { redirectTo?: string; minRole?: Role } = {}) {
   const user = useContext(AuthContext);
 
   useEffect(() => {
-    if (redirectTo && !user) void Router.push(redirectTo);
-  }, [user, redirectTo]);
+    if (user) {
+      switch (user.role) {
+        case Role.Superuser:
+          return;
+        case Role.Staff:
+          if (minRole === Role.Staff) return;
+        case Role.Premium:
+          if (minRole === Role.Premium) return;
+        case Role.Basic:
+          if (minRole === Role.Basic) return;
+        default:
+          void Router.push(redirectTo);
+      }
+    }
+    void Router.push(redirectTo);
+  }, [user, redirectTo, minRole]);
 
   return user;
 }
