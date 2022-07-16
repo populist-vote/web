cb6c851bb8428b7e02f9dbb03eca8053a19b39d4
Wiley Conte <wileymckayconte@gmail.com>
2022-07-16 11:23:26 -0500

    Refactor nav for flexibilitY

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,15 +2,37 @@ import type { PropsWithChildren } from "react";
 import { Footer, Nav } from "components";
 import styles from "./Layout.module.scss";
 
+const defaultNavItems = [
+  {
+    label: "Ballot",
+    href: "/ballot",
+  },
+  {
+    label: "Politicians",
+    href: "/politicians",
+  },
+  {
+    label: "Voting guides",
+    href: "/voting-guides",
+  },
+];
+
 function Layout({
   mobileNavTitle,
   showNavBackButton = false,
   showNavLogoOnMobile = true,
+  navItems = defaultNavItems,
+  hideFooter = false,
   children,
 }: PropsWithChildren<{
   mobileNavTitle?: string;
   showNavBackButton?: boolean;
   showNavLogoOnMobile?: boolean;
+  navItems?: {
+    label: string;
+    href: string;
+  }[];
+  hideFooter?: boolean;
 }>) {
   return (
     <div className={styles.app}>
@@ -19,10 +41,11 @@ function Layout({
           mobileNavTitle={mobileNavTitle}
           showBackButton={showNavBackButton}
           showLogoOnMobile={showNavLogoOnMobile}
+          navItems={navItems}
         />
       </aside>
       <main className={styles.content}>{children}</main>
-      <Footer />
+      {!hideFooter && <Footer />}
     </div>
   );
 }
