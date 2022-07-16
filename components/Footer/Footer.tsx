649e21bc4f00b4caa177fcbc4e8ab8f2e0b78242
Wiley Conte <wileymckayconte@gmail.com>
2022-07-15 23:02:28 -0500

    Footer css tweaks

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,42 +5,53 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>
-        <ul>
-          <li>
-            <Link href="/about" passHref>
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link href="/faq" passHref>
-              FAQ
-            </Link>
-          </li>
-        </ul>
-        <a
-          href="https://www.facebook.com/populist.us"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <FaFacebookF />
-        </a>
-        <a
-          href="https://twitter.com/populist_us"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <FaTwitter />
-        </a>
-        <a
-          href="https://www.instagram.com/populist.us/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <FaInstagram />
-        </a>
-      </p>
-      <p id="copyright">
+      <ul>
+        <li>
+          <Link href="/about" passHref>
+            ABOUT
+          </Link>
+        </li>
+        <li>
+          <Link href="/faq" passHref>
+            FAQ
+          </Link>
+        </li>
+      </ul>
+      <ul className={styles.socials}>
+        <li>
+          {" "}
+          <a
+            href="https://www.facebook.com/populist.us"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaFacebookF />
+          </a>
+        </li>
+
+        <li>
+          {" "}
+          <a
+            href="https://twitter.com/populist_us"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaTwitter />
+          </a>
+        </li>
+
+        <li>
+          {" "}
+          <a
+            href="https://www.instagram.com/populist.us/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaInstagram />
+          </a>
+        </li>
+      </ul>
+      <p className={styles.copywright}>
         &#169; {new Date().getFullYear()} Populist. &nbsp;Made in Colorado.
       </p>
     </footer>
