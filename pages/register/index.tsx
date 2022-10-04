24b6d8bfb29ef3b373c2d0156b005a421688d128
Wiley Conte <wileymckayconte@gmail.com>
2022-10-04 14:26:44 -0600

    Improve post registration flow

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,7 +1,12 @@
 import { StateMachineProvider, createStore } from "little-state-machine";
-import { BeginUserRegistrationInput, State } from "generated";
+import {
+  BeginUserRegistrationInput,
+  State,
+  useCurrentUserQuery,
+} from "generated";
 import { EmailStep } from "components/Auth/Register/EmailStep";
-import { BasicLayout } from "components";
+import { BasicLayout, LoaderFlag } from "components";
+import { useRouter } from "next/router";
 
 export const updateAction = (
   state: { loginFormState: BeginUserRegistrationInput },
@@ -17,6 +22,11 @@ export const updateAction = (
 };
 
 function Register() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrentUserQuery();
+  const user = data?.currentUser;
+  if (user) void router.push(`/${router.query.next || "/home"}`);
+
   createStore({
     loginFormState: {
       email: "",
@@ -31,6 +41,8 @@ function Register() {
       },
     },
   });
+
+  if (isLoading) return <LoaderFlag height={100} />;
 
   return (
     <StateMachineProvider>
