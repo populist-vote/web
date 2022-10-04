34cdc8c9b5d8a4725f34d91ee1bdae4b74f553d8
Wiley Conte <wileymckayconte@gmail.com>
2022-10-04 13:22:41 -0600

    Switch to pnpm and remove cypress and storybook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
