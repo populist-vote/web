472d5176c76262f44b9826e3adac91f6963fa659
Wiley Conte <wileymckayconte@gmail.com>
2022-10-05 15:29:07 -0600

    Add SEO tag to MPR page

--- pages/mpr/index.tsx
+++ pages/mpr/index.tsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import { NextPage } from "next";
 
-import { Layout, MPRLogo, LoaderFlag, RaceSection, Button } from "components";
+import {
+  Layout,
+  MPRLogo,
+  LoaderFlag,
+  RaceSection,
+  Button,
+  SEO,
+} from "components";
 import { useMprFeaturedRacesQuery, RaceResult } from "generated";
 import styles from "./MPRElectionPage.module.scss";
 import { splitRaces } from "utils/data";
@@ -27,67 +34,97 @@ const MPRElection: NextPage = () => {
     );
 
   return (
-    <Layout>
-      <div className={styles.mprHeader}>
-        <h1 className={styles.mprLogo}>
-          <MPRLogo height={80} />
-        </h1>
-        <h1 className={styles.subHeading}>Build Your 2022 MN Election Ballot</h1>
-        <h4 className={styles.racesHeading}>Highlighted Races</h4>
-        
-        <p>MPR News has highlighted some of the important races happening in Minnesota this election - check out the candidates below, then view what's on your ballot and create your own personalized voting guide to share with others.</p>
-        <p className={styles.cta}><a href="../ballot"><Button variant="primary" size="large" label="View your ballot"></Button></a></p>
-        <p>
-          Got more questions about the election? {" "} 
-          <Link
-            href="https://www.mprnews.org/election-2022-questions-stories"
-            target="_blank"
-            rel="noreferrer"
-          >
-          Share them with us</Link>, and stay informed on the latest news about who is running and what Minnesotans are looking for in our daily{" "}
-          <Link
-            href="https://www.mprnews.org/capitol-view-newsletter"
-            target="_blank"
-            rel="noreferrer"
-          >Capitol View newsletter.</Link>
-        </p>
-        
-      </div>
+    <>
+      <SEO title="MPR News | Minnesota 2022 Election" />
+      <Layout>
+        <div className={styles.mprHeader}>
+          <h1 className={styles.mprLogo}>
+            <MPRLogo height={80} />
+          </h1>
+          <h1 className={styles.subHeading}>
+            Build Your 2022 MN Election Ballot
+          </h1>
+          <h4 className={styles.racesHeading}>Highlighted Races</h4>
 
-      {isLoading ? (
-        <LoaderFlag />
-      ) : (
-        <>
-          {races?.state && (
-            <RaceSection races={races.state} color="yellow" title="State" />
-          )}
-          <div className={styles.mprBody}>
           <p>
-            Early voting is open now until November 8.{" "} 
+            MPR News has highlighted some of the important races happening in
+            Minnesota this election - check out the candidates below, then view
+            what's on your ballot and create your own personalized voting guide
+            to share with others.
+          </p>
+          <p className={styles.cta}>
+            <a href="../ballot">
+              <Button
+                variant="primary"
+                size="large"
+                label="View your ballot"
+              ></Button>
+            </a>
+          </p>
+          <p>
+            Got more questions about the election?{" "}
             <Link
-              href="https://www.sos.state.mn.us/elections-voting/other-ways-to-vote/vote-early-by-mail/"
+              href="https://www.mprnews.org/election-2022-questions-stories"
               target="_blank"
               rel="noreferrer"
             >
-              Request a mail-in ballot</Link> or find out where to vote{" "} 
+              Share them with us
+            </Link>
+            , and stay informed on the latest news about who is running and what
+            Minnesotans are looking for in our daily{" "}
             <Link
-              href="https://pollfinder.sos.state.mn.us/"
+              href="https://www.mprnews.org/capitol-view-newsletter"
               target="_blank"
               rel="noreferrer"
-            >here</Link> — if you aren’t registered to vote, you can{" "} 
-            <Link
-              href="https://mnvotes.sos.state.mn.us/VoterRegistration/VoterRegistrationMain.aspx"
-              target="_blank"
-              rel="noreferrer"
-            >register online</Link>.
+            >
+              Capitol View newsletter.
+            </Link>
           </p>
-          </div>
-          {races?.federal && (
-            <RaceSection races={races.federal} color="aqua" title="Federal" />
-          )}
-        </>
-      )}
-    </Layout>
+        </div>
+
+        {isLoading ? (
+          <LoaderFlag />
+        ) : (
+          <>
+            {races?.state && (
+              <RaceSection races={races.state} color="yellow" title="State" />
+            )}
+            <div className={styles.mprBody}>
+              <p>
+                Early voting is open now until November 8.{" "}
+                <Link
+                  href="https://www.sos.state.mn.us/elections-voting/other-ways-to-vote/vote-early-by-mail/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Request a mail-in ballot
+                </Link>{" "}
+                or find out where to vote{" "}
+                <Link
+                  href="https://pollfinder.sos.state.mn.us/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  here
+                </Link>{" "}
+                — if you aren’t registered to vote, you can{" "}
+                <Link
+                  href="https://mnvotes.sos.state.mn.us/VoterRegistration/VoterRegistrationMain.aspx"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  register online
+                </Link>
+                .
+              </p>
+            </div>
+            {races?.federal && (
+              <RaceSection races={races.federal} color="aqua" title="Federal" />
+            )}
+          </>
+        )}
+      </Layout>
+    </>
   );
 };
 
