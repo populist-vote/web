a236e38e675290f8f31f47c74adb8509d8560c7a
Henry Lai <lai.henry@gmail.com>
2022-10-05 13:10:44 -0600

    mpr copy edits

--- pages/mpr/index.tsx
+++ pages/mpr/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { NextPage } from "next";
 
-import { Layout, MPRLogo, LoaderFlag, RaceSection } from "components";
+import { Layout, MPRLogo, LoaderFlag, RaceSection, Button } from "components";
 import { useMprFeaturedRacesQuery, RaceResult } from "generated";
 import styles from "./MPRElectionPage.module.scss";
 import { splitRaces } from "utils/data";
@@ -32,26 +32,24 @@ const MPRElection: NextPage = () => {
         <h1 className={styles.mprLogo}>
           <MPRLogo height={80} />
         </h1>
-        <h1 className={styles.subHeading}>2022 Election Coverage</h1>
+        <h1 className={styles.subHeading}>Build Your 2022 MN Election Ballot</h1>
         <h4 className={styles.racesHeading}>Highlighted Races</h4>
-        <p className={styles.racesDescription}>
-          Early voting is open now until November 8.{" "} 
+        
+        <p>MPR News has highlighted some of the important races happening in Minnesota this election - check out the candidates below, then view what's on your ballot and create your own personalized voting guide to share with others.</p>
+        <p><a href="../ballot"><Button variant="primary" size="large" label="View your ballot"></Button></a></p>
+        <p>
+          Got more questions about the election? {" "} 
           <Link
-            href="https://www.sos.state.mn.us/elections-voting/other-ways-to-vote/vote-early-by-mail/"
+            href="https://www.mprnews.org/election-2022-questions-stories"
             target="_blank"
             rel="noreferrer"
           >
-            Request a mail-in ballot</Link> or find out where to vote{" "} 
+          Share them with us</Link>, and stay informed on the latest news about who is running and what Minnesotans are looking for in our daily{" "}
           <Link
-            href="https://pollfinder.sos.state.mn.us/"
+            href="https://www.mprnews.org/capitol-view-newsletter"
             target="_blank"
             rel="noreferrer"
-          >here</Link> — if you aren’t registered to vote, you can{" "} 
-          <Link
-            href="https://mnvotes.sos.state.mn.us/VoterRegistration/VoterRegistrationMain.aspx"
-            target="_blank"
-            rel="noreferrer"
-          >register online</Link>.
+          >Capitol View newsletter.</Link>
         </p>
         
       </div>
@@ -64,20 +62,25 @@ const MPRElection: NextPage = () => {
             <RaceSection races={races.state} color="yellow" title="State" />
           )}
           <div className={styles.mprBody}>
-            <p className={styles.racesDescription}>
-              MPR News is here to serve your election information needs. {" "} 
-              <Link
-                href="https://www.mprnews.org/election-2022-questions-stories"
-                target="_blank"
-                rel="noreferrer"
-              >
-              What do you want to know about the 2022 election in Minnesota?</Link> You can also stay informed on the latest news about who is running and what Minnesotans are looking for in our daily{" "}
-              <Link
-                href="https://www.mprnews.org/capitol-view-newsletter"
-                target="_blank"
-                rel="noreferrer"
-              >Capitol View newsletter.</Link>
-            </p>
+          <p>
+            Early voting is open now until November 8.{" "} 
+            <Link
+              href="https://www.sos.state.mn.us/elections-voting/other-ways-to-vote/vote-early-by-mail/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Request a mail-in ballot</Link> or find out where to vote{" "} 
+            <Link
+              href="https://pollfinder.sos.state.mn.us/"
+              target="_blank"
+              rel="noreferrer"
+            >here</Link> — if you aren’t registered to vote, you can{" "} 
+            <Link
+              href="https://mnvotes.sos.state.mn.us/VoterRegistration/VoterRegistrationMain.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >register online</Link>.
+          </p>
           </div>
           {races?.federal && (
             <RaceSection races={races.federal} color="aqua" title="Federal" />
