a180839f23a46929fa433609d5cbf7f167efdd34
Wiley Conte <wileymckayconte@gmail.com>
2022-10-10 11:26:15 -0600

    Change url for sharing guides

--- pages/voting-guides/index.tsx
+++ pages/voting-guides/index.tsx
@@ -27,7 +27,7 @@ import { PERSON_FALLBACK_IMAGE_URL } from "utils/constants";
 import styles from "./VotingGuides.module.scss";
 
 const getGuideUrl = (guideId: string) =>
-  `${window.location.origin}/ballot?voting-guide=${guideId}`;
+  `${window.location.origin}/voting-guides/${guideId}`;
 
 const copyGuideUrl = (guideId?: string) => {
   if (!guideId) return;
