de6debbd48a6191c77eb8e3d77481a04ecd5fe2b
Wiley Conte <wileymckayconte@gmail.com>
2022-10-10 11:33:39 -0600

    Add SSR support for voting guide id page

--- pages/voting-guides/[id].tsx
+++ pages/voting-guides/[id].tsx
@@ -1,11 +1,14 @@
 import { Layout, LoaderFlag, SEO } from "components";
 import { Election } from "components/Ballot/Election";
-import { useVotingGuideByIdQuery } from "generated";
+import { useVotingGuideByIdQuery, VotingGuideByIdQuery } from "generated";
 import { useAuth } from "hooks/useAuth";
 import { useSavedGuideIds } from "hooks/useSavedGuideIds";
 import { VotingGuideProvider } from "hooks/useVotingGuide";
+import { GetServerSideProps } from "next";
+import { NextParsedUrlQuery } from "next/dist/server/request-meta";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { dehydrate, QueryClient } from "react-query";
 
 export default function VotingGuidePage() {
   const user = useAuth({ redirect: false });
@@ -52,3 +55,33 @@ export default function VotingGuidePage() {
     </>
   );
 }
+
+interface Params extends NextParsedUrlQuery {
+  id: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.query as Params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    useVotingGuideByIdQuery.getKey({ id }),
+    useVotingGuideByIdQuery.fetcher({ id })
+  );
+
+  const state = dehydrate(queryClient);
+
+  const data = state.queries[0]?.state.data as VotingGuideByIdQuery;
+  const guide = data?.votingGuideById;
+  const mobileNavTitle = `${
+    guide?.user?.firstName || guide?.user?.username
+  }'s Voting Guide`;
+
+  return {
+    notFound: !data,
+    props: {
+      dehydratedState: state,
+      mobileNavTitle,
+    },
+  };
+};
