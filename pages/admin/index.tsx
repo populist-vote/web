11ac4e2e38ecdbaa6e25f7180def16fa56e6cf44
Wiley Conte <wileymckayconte@gmail.com>
2022-10-19 16:07:42 -0600

    Add users by state to admin panel

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,33 +2,43 @@
 import type { NextPage } from "next";
 import { Layout } from "components";
 import { useAuth } from "hooks/useAuth";
-import { Role, useUserCountQuery } from "generated";
+import { Role, State, useUserCountByStateQuery } from "generated";
 import styles from "./admin.module.scss";
 import classNames from "classnames";
 
 const adminNavItems = [
   {
-    label: "Users",
-    href: "/admin/users",
-  },
-  {
-    label: "Data",
-    href: "/admin/data",
+    label: "Home",
+    href: "/home",
   },
 ];
 
 const AdminPanel: NextPage = () => {
   useAuth({ redirectTo: "/login", minRole: Role.Staff });
 
-  const { data } = useUserCountQuery();
+  const { data: totalUserCount } = useUserCountByStateQuery();
+  const { data: coUserCount } = useUserCountByStateQuery({ state: State.Co });
+  const { data: mnUserCount } = useUserCountByStateQuery({ state: State.Mn });
 
   return (
     <Layout navItems={adminNavItems} hideFooter>
       <div className={styles.adminContainer}>
-        <div className={classNames(styles.flexBetween, styles.adminBox)}>
-          <span className={styles.itemHeader}>Total Users</span>
-          <span className={styles.dots} />
-          <h1>{data?.userCount}</h1>
+        <div className={classNames(styles.adminBox)}>
+          <div className={styles.flexBetween}>
+            <span className={styles.itemHeader}>Total Users</span>
+            <span className={styles.dots} />
+            <h1>{totalUserCount?.userCount}</h1>
+          </div>
+          <div className={styles.flexBetween}>
+            <span className={styles.itemHeader}>Colorado</span>
+            <span className={styles.dots} />
+            <h2>{coUserCount?.userCount}</h2>
+          </div>
+          <div className={styles.flexBetween}>
+            <span className={styles.itemHeader}>Minnesota</span>
+            <span className={styles.dots} />
+            <h2>{mnUserCount?.userCount}</h2>
+          </div>
         </div>
       </div>
     </Layout>
