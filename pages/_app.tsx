518061efae32cb6e3f8cd3246124e1baab6f084e
Wiley Conte <wileymckayconte@gmail.com>
2022-10-28 12:37:15 -0600

    Test23

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,34 +17,34 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Hydrate state={pageProps.dehydratedState}>
-            <Head>
-              <SEO {...pageProps} />
-            </Head>
-            {process.env.NODE_ENV === "production" &&
-              process.env.GOOGLE_ANALYTICS_ID && (
-                <>
-                  <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-                    strategy="afterInteractive"
-                  />
-                  <Script id="google-analytics" strategy="afterInteractive">
-                    {`
+      <Head>
+        <SEO {...pageProps} />
+      </Head>
+      {process.env.NODE_ENV === "production" &&
+        process.env.GOOGLE_ANALYTICS_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){window.dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', "${process.env.GOOGLE_ANALYTICS_ID}");
               `}
-                  </Script>
-                </>
-              )}
+            </Script>
+          </>
+        )}
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <AuthProvider>
             <Component {...pageProps} />
             <ToastContainer theme="dark" />
             <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
-          </Hydrate>
-        </AuthProvider>
+          </AuthProvider>
+        </Hydrate>
       </QueryClientProvider>
     </>
   );
