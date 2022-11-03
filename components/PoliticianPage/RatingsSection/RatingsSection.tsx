42c743e5507d7e88e0e8f1d20a26b862363e80df
Wiley Conte <wileymckayconte@gmail.com>
2022-11-03 12:42:45 -0600

    Make ratings clickable

--- components/PoliticianPage/RatingsSection/RatingsSection.tsx
+++ components/PoliticianPage/RatingsSection/RatingsSection.tsx
@@ -7,6 +7,7 @@ import {
   usePoliticianRatingsQuery,
 } from "generated";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { ORGANIZATION_FALLBACK_IMAGE_URL } from "utils/constants";
 import styles from "./RatingsSection.module.scss";
@@ -19,37 +20,40 @@ function RatingsItem({ rating }: { rating: RatingResult; itemId: string }) {
   const ratingPercent = parseInt(rating.vsRating.rating);
 
   return (
-    <div className={styles.ratingContainer} key={rating?.vsRating.ratingId}>
-      <div className={styles.circlesContainer}>
-        <OrganizationAvatar
-          src={
-            (rating.organization?.assets?.thumbnailImage160 ||
-              rating.organization?.thumbnailImageUrl) as string
-          }
-          fallbackSrc={ORGANIZATION_FALLBACK_IMAGE_URL}
-          alt={rating?.organization?.name as string}
-          size={80}
-        />
-        <div
-          className={styles.ratingCircle}
-          style={{
-            background: `${
-              ratingPercent > 80
-                ? "var(--green)"
-                : ratingPercent > 50
-                ? "var(--yellow)"
-                : "var(--red)"
-            }`,
-          }}
-        >
-          <span>{ratingPercent}</span>
+    <Link href={`/organizations/${rating.organization?.slug}`}>
+      <div className={styles.ratingContainer} key={rating?.vsRating.ratingId}>
+        <div className={styles.circlesContainer}>
+          <OrganizationAvatar
+            src={
+              (rating.organization?.assets?.thumbnailImage160 ||
+                rating.organization?.thumbnailImageUrl) as string
+            }
+            fallbackSrc={ORGANIZATION_FALLBACK_IMAGE_URL}
+            alt={rating?.organization?.name as string}
+            size={80}
+          />
+
+          <div
+            className={styles.ratingCircle}
+            style={{
+              background: `${
+                ratingPercent > 80
+                  ? "var(--green)"
+                  : ratingPercent > 50
+                  ? "var(--yellow)"
+                  : "var(--red)"
+              }`,
+            }}
+          >
+            <span>{ratingPercent}</span>
+          </div>
         </div>
+        {rating.organization?.name && (
+          <span className={styles.avatarName}>{rating.organization?.name}</span>
+        )}
+        <span className={styles.timespan}>{rating.vsRating.timespan}</span>
       </div>
-      {rating.organization?.name && (
-        <span className={styles.avatarName}>{rating.organization?.name}</span>
-      )}
-      <span className={styles.timespan}>{rating.vsRating.timespan}</span>
-    </div>
+    </Link>
   );
 }
 
