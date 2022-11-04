31ccdfcd3a746b8885133dd6a2c22072b24e90e6
Zack <zackrose@gmail.com>
2022-11-03 17:41:44 -0700

    add language constants

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,3 +1,29 @@
+// import { i18n } from "next-i18next.config";
+// export const DEFAULT_LANGUAGE = i18n.defaultLocale;
+// export type Language = typeof i18n.locales[number];
+
+//These are temporary until it can be automated (typescript doesn't like the above defs)
+export const DEFAULT_LANGUAGE = "en";
+export type Language = "en" | "es" | "so" | "hmn";
+export interface LanguageNote {
+  en?: string;
+  es?: string;
+  so?: string;
+  hmn?: string;
+}
+
+export interface LanguageCode {
+  code: Language;
+  display: string;
+}
+
+export const Languages: LanguageCode[] = [
+  { code: "en", display: "English" },
+  { code: "es", display: "Spanish" },
+  { code: "so", display: "Somali" },
+  { code: "hmn", display: "Hmong" },
+];
+
 // Urls
 
 export const PERSON_FALLBACK_IMAGE_URL =
