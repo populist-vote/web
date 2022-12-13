3914c1863f5d0f8b2135953d514e69f7ffbed0ab
Wiley Conte <wileymckayconte@gmail.com>
2022-12-12 21:07:12 -0500

    Revert "Add Filters icon"
    This reverts commit 1e154ed2681d0ff5c152965efec84790b4478627.

--- components/Icons/FiltersIcon.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/legacy/image";
-import FiltersIconSVG from "public/images/FiltersIcon.svg";
-
-export default function NoteIcon({ ...rest }) {
-  return <Image src={FiltersIconSVG} alt="Filters" priority {...rest} />;
-}
