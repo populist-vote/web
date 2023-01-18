988b90c5eac31f14367e96be66ba3154c66b1646
Wiley Conte <wileymckayconte@gmail.com>
2023-01-17 21:16:18 -0700

    Ad i18n props

--- pages/[slug]/embeds/new.tsx
+++ pages/[slug]/embeds/new.tsx
@@ -1,7 +1,26 @@
 import { Layout, TextInput } from "components";
+import nextI18nextConfig from "next-i18next.config";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import { SupportedLocale } from "types/global";
 import { dashboardNavItems } from "utils/nav";
+
+export async function getServerSideProps({
+  locale,
+}: {
+  locale: SupportedLocale;
+}) {
+  return {
+    props: {
+      ...(await serverSideTranslations(
+        locale,
+        ["auth", "common"],
+        nextI18nextConfig
+      )),
+    },
+  };
+}
 
 function EmbedsNew() {
   const router = useRouter();
--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -1,11 +1,25 @@
 import { Layout } from "components";
+import nextI18nextConfig from "next-i18next.config";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
+import { SupportedLocale } from "types/global";
 import { dashboardNavItems } from "utils/nav";
 
-export function getServerSideProps({ query }: { query: { slug: string } }) {
+export async function getServerSideProps({
+  query,
+  locale,
+}: {
+  query: { slug: string };
+  locale: SupportedLocale;
+}) {
   return {
     props: {
       slug: query.slug,
+      ...(await serverSideTranslations(
+        locale,
+        ["auth", "common"],
+        nextI18nextConfig
+      )),
     },
   };
 }
