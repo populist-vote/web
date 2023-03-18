2c00ac906c4293450b1b58202aba9b41f6a63d5a
Zack <zackrose@gmail.com>
2023-03-18 12:39:19 -0700

    New Home Changes

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,14 @@
 import { ReactNode, useState } from "react";
-import { BasicLayout, HomePageButton, BetaNotice } from "components";
-import { BETA_NOTICE_VISIBLE } from "utils/constants";
-import { useAuth } from "hooks/useAuth";
-import { SupportedLocale } from "types/global";
+import { useRouter } from "next/router";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import nextI18nextConfig from "next-i18next.config";
 import { useTranslation } from "next-i18next";
+
+import { Layout, HomePageButton, BetaNotice, LoaderFlag } from "components";
+import { BETA_NOTICE_VISIBLE } from "utils/constants";
+import { useAuth } from "hooks/useAuth";
+import { SupportedLocale } from "types/global";
+import { useOrganizationByIdQuery } from "generated";
 
 export async function getServerSideProps({
   locale,
@@ -24,35 +27,17 @@ export async function getServerSideProps({
   };
 }
 
-function HomePage() {
-  const { user } = useAuth({ redirect: false });
-  const userId = user?.id;
+function CitizenHome({ userLoggedIn }: { userLoggedIn: boolean }) {
   const { t } = useTranslation("common");
-
-  const [isBetaVisible, setIsBetaVisible] = useState(
-    localStorage.getItem(`${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`) !==
-      "false"
-  );
-
-  const handleBetaDismissal = () => {
-    setIsBetaVisible(false);
-    localStorage.setItem(
-      `${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`,
-      "false"
-    );
-  };
-
-  return isBetaVisible ? (
-    <BetaNotice onContinue={handleBetaDismissal} />
-  ) : (
+  return (
     <div>
       <HomePageButton
-        href={user ? "/ballot" : "/ballot/choose"}
+        href={userLoggedIn ? "/ballot" : "/ballot/choose"}
         className="myBallot"
         label={t("my-ballot")}
       />
       <HomePageButton
-        href={user ? "/voting-guides" : "/login?next=/voting-guides"}
+        href={userLoggedIn ? "/voting-guides" : "/login?next=/voting-guides"}
         className="votingGuides"
         label={t("voting-guides")}
       />
@@ -70,8 +55,50 @@ function HomePage() {
   );
 }
 
-HomePage.getLayout = (page: ReactNode) => (
-  <BasicLayout showAuthButtons>{page}</BasicLayout>
-);
+function HomePage() {
+  const { user } = useAuth({ redirect: false });
+  const userId = user?.id;
+  const organizationId = user?.organizationId;
+  const userType = organizationId !== undefined ? "Organization" : "Citizen";
+
+  const { data, isLoading } = useOrganizationByIdQuery(
+    {
+      id: user?.organizationId || "",
+    },
+    {
+      enabled: !!user?.organizationId,
+    }
+  );
+
+  const organization = data?.organizationById;
+
+  const router = useRouter();
+
+  const [isBetaVisible, setIsBetaVisible] = useState(
+    localStorage.getItem(`${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`) !==
+      "false"
+  );
+
+  const handleBetaDismissal = () => {
+    setIsBetaVisible(false);
+    localStorage.setItem(
+      `${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`,
+      "false"
+    );
+  };
+
+  if (userType === "Organization") {
+    if (isLoading) return <LoaderFlag />;
+    else if (organization) void router.push(`/dashboard/${organization.slug}`);
+  }
+
+  return isBetaVisible ? (
+    <BetaNotice onContinue={handleBetaDismissal} />
+  ) : (
+    <CitizenHome userLoggedIn={!!user} />
+  );
+}
+
+HomePage.getLayout = (page: ReactNode) => <Layout>{page}</Layout>;
 
 export default HomePage;
