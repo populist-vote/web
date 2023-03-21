8f7125cbd6466a74a0769bfac5d48d50e76fd01f
Zack <zackrose@gmail.com>
2023-03-20 18:25:07 -0700

    remove home redirect to dashboard

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,14 +1,12 @@
 import { ReactNode, useState } from "react";
-import { useRouter } from "next/router";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import nextI18nextConfig from "next-i18next.config";
 import { useTranslation } from "next-i18next";
 
-import { Layout, HomePageButton, BetaNotice, LoaderFlag } from "components";
+import { Layout, HomePageButton, BetaNotice } from "components";
 import { BETA_NOTICE_VISIBLE } from "utils/constants";
 import { useAuth } from "hooks/useAuth";
 import { SupportedLocale } from "types/global";
-import { useOrganizationByIdQuery } from "generated";
 
 export async function getServerSideProps({
   locale,
@@ -58,21 +56,6 @@ function CitizenHome({ userLoggedIn }: { userLoggedIn: boolean }) {
 function HomePage() {
   const { user } = useAuth({ redirect: false });
   const userId = user?.id;
-  const organizationId = user?.organizationId;
-  const userType = organizationId !== undefined ? "Organization" : "Citizen";
-
-  const { data, isLoading } = useOrganizationByIdQuery(
-    {
-      id: user?.organizationId || "",
-    },
-    {
-      enabled: !!user?.organizationId,
-    }
-  );
-
-  const organization = data?.organizationById;
-
-  const router = useRouter();
 
   const [isBetaVisible, setIsBetaVisible] = useState(
     localStorage.getItem(`${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`) !==
@@ -86,11 +69,6 @@ function HomePage() {
       "false"
     );
   };
-
-  if (userType === "Organization") {
-    if (isLoading) return <LoaderFlag />;
-    else if (organization) void router.push(`/dashboard/${organization.slug}`);
-  }
 
   return isBetaVisible ? (
     <BetaNotice onContinue={handleBetaDismissal} />
