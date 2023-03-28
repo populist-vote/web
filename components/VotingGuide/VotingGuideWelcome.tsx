fcf9fed8fa59c62ee12ab851d8ff8d913fa91847
Wiley Conte <wileymckayconte@gmail.com>
2023-03-28 15:14:10 -0600

    Welcome box styles

--- components/VotingGuide/VotingGuideWelcome.tsx
+++ components/VotingGuide/VotingGuideWelcome.tsx
@@ -1,17 +1,16 @@
 import { Button, BasicLayout, TopNavElections } from "components";
-import styles from "styles/modules/page.module.scss";
-import VotingGuideWelcomeStyles from "./VotingGuideWelcome.module.scss";
+import styles from "./VotingGuideWelcome.module.scss";
 import clsx from "clsx";
 import { ReactNode } from "react";
+import { Box } from "components/Box/Box";
 
 function VotingGuideWelcome({ onClose }: { onClose: () => void }) {
   return (
     <>
       <TopNavElections selected="Ballot" />
-      <div className={styles.container}>
-        <section
-          className={clsx(styles.center, VotingGuideWelcomeStyles.content)}
-        >
+
+      <section className={clsx(styles.content)}>
+        <Box>
           <h1>Create A Voting Guide</h1>
           <p>
             Endorse and comment on candidates running for local offices to
@@ -26,8 +25,8 @@ function VotingGuideWelcome({ onClose }: { onClose: () => void }) {
             size="large"
             onClick={onClose}
           />
-        </section>
-      </div>
+        </Box>
+      </section>
     </>
   );
 }
