60d59c2def0b34d44b44f7043ea0c7ffbe620203
Wiley Conte <wileymckayconte@gmail.com>
2023-03-29 19:17:37 -0600

    Iframe with ID

--- scripts/widget-client.ts
+++ scripts/widget-client.ts
@@ -27,9 +27,10 @@
   const loading = attributes.loading === "lazy" ? "lazy" : undefined;
 
   // Set up iframe element
-  const iframeElement = document.createElement(`iframe-${params.embedId}`);
+  const iframeElement = document.createElement("iframe");
   const iframeAttributes = {
     class: "populist-frame populist-frame--loading",
+    id: `populist-iframe-${params.embedId}`,
     title: "Populist Widget",
     scrolling: "no",
     allow: "clipboard-write",
