32060af0155563549cd7ffa3255af9f601f0cf81
Wiley Conte <wileymckayconte@gmail.com>
2023-04-10 11:28:08 -0600

    Hide last vote when none exists

--- components/BillWidget/LastVoteSection/LastVoteSection.tsx
+++ components/BillWidget/LastVoteSection/LastVoteSection.tsx
@@ -15,70 +15,74 @@ function LastVoteSection({ votes }: { votes: Bill["votes"] }) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.chamberVote}>
-        <h4>House</h4>
-        <div className={styles.pieChartContainer}>
-          <PieChart
-            data={[
-              {
-                title: "Yea",
-                value: lastHouseVote?.yea as number,
-                color: "var(--green-support)",
-              },
-              {
-                title: "Nay",
-                value: lastHouseVote?.nay as number,
-                color: "var(--red)",
-              },
-            ]}
-            lineWidth={22}
-            style={{ height: "80px" }}
-            className={styles.pieChart}
-          />
-          <div className={styles.votes}>
-            <div className={styles.vote}>
-              <FaCheckCircle size={13} color="var(--green-support)" />
-              <span>{lastHouseVote?.yea}</span>
-            </div>
-            <div className={styles.vote}>
-              <FaTimesCircle size={13} color="var(--red)" />
-              <span>{lastHouseVote?.nay}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.chamberVote}>
-        <h4>Senate</h4>
-        <div className={styles.pieChartContainer}>
-          <PieChart
-            data={[
-              {
-                title: "Yea",
-                value: lastSenateVote?.yea as number,
-                color: "var(--green-support)",
-              },
-              {
-                title: "Nay",
-                value: lastSenateVote?.nay as number,
-                color: "var(--red)",
-              },
-            ]}
-            lineWidth={22}
-            style={{ height: "80px" }}
-            className={styles.pieChart}
-          />
-          <div className={styles.votes}>
-            <div className={styles.vote}>
-              <FaCheckCircle size={13} color="var(--green-support)" />
-              <span>{lastSenateVote?.yea}</span>
-            </div>
-            <div className={styles.vote}>
-              <FaTimesCircle size={13} color="var(--red)" />
-              <span>{lastSenateVote?.nay}</span>
+      {lastHouseVote && (
+        <div className={styles.chamberVote}>
+          <h4>House</h4>
+          <div className={styles.pieChartContainer}>
+            <PieChart
+              data={[
+                {
+                  title: "Yea",
+                  value: lastHouseVote?.yea as number,
+                  color: "var(--green-support)",
+                },
+                {
+                  title: "Nay",
+                  value: lastHouseVote?.nay as number,
+                  color: "var(--red)",
+                },
+              ]}
+              lineWidth={22}
+              style={{ height: "80px" }}
+              className={styles.pieChart}
+            />
+            <div className={styles.votes}>
+              <div className={styles.vote}>
+                <FaCheckCircle size={13} color="var(--green-support)" />
+                <span>{lastHouseVote?.yea}</span>
+              </div>
+              <div className={styles.vote}>
+                <FaTimesCircle size={13} color="var(--red)" />
+                <span>{lastHouseVote?.nay}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+      {lastSenateVote && (
+        <div className={styles.chamberVote}>
+          <h4>Senate</h4>
+          <div className={styles.pieChartContainer}>
+            <PieChart
+              data={[
+                {
+                  title: "Yea",
+                  value: lastSenateVote?.yea as number,
+                  color: "var(--green-support)",
+                },
+                {
+                  title: "Nay",
+                  value: lastSenateVote?.nay as number,
+                  color: "var(--red)",
+                },
+              ]}
+              lineWidth={22}
+              style={{ height: "80px" }}
+              className={styles.pieChart}
+            />
+            <div className={styles.votes}>
+              <div className={styles.vote}>
+                <FaCheckCircle size={13} color="var(--green-support)" />
+                <span>{lastSenateVote?.yea}</span>
+              </div>
+              <div className={styles.vote}>
+                <FaTimesCircle size={13} color="var(--red)" />
+                <span>{lastSenateVote?.nay}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
