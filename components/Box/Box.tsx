7911345db2b4dcce2bd7fc82c2693b932aef4ead
Wiley Conte <wileymckayconte@gmail.com>
2023-05-24 16:04:51 -0600

    Add poll activity dashboard

--- components/Box/Box.tsx
+++ components/Box/Box.tsx
@@ -5,6 +5,7 @@ type BoxProps = PropsWithChildren<{
   width?: string;
   padding?: string;
   isLink?: boolean;
+  flexDirection?: "row" | "column";
 }>;
 
 function Box({
@@ -12,16 +13,19 @@ function Box({
   width = "100%",
   padding = "2rem",
   isLink = false,
+  flexDirection = "column",
   ...rest
 }: BoxProps) {
   const styleVars: CSSProperties & {
     "--box-width"?: string;
     "--box-padding"?: string;
     "--box-hover-shadow"?: string;
+    "--flex-direction"?: "row" | "column";
   } = {
     "--box-width": width,
     "--box-padding": padding,
     "--box-hover-shadow": isLink ? "0 0 0 1px var(--blue)" : "none",
+    "--flex-direction": flexDirection,
   };
   return (
     <div className={styles.container} style={styleVars} {...rest}>
