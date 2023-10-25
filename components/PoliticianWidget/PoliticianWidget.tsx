aee21b8bc18b715c4c70469578370247650a3eaa
Wiley Conte <wileymckayconte@gmail.com>
2023-10-25 14:32:22 -0600

    Conditional rendering of endorsements and socials

--- components/PoliticianWidget/PoliticianWidget.tsx
+++ components/PoliticianWidget/PoliticianWidget.tsx
@@ -148,178 +148,215 @@ export function PoliticianWidget({
     </section>
   );
 
-  const $endorsementSection = (
-    <section className={styles.endorsementSection}>
-      <h4>Endorsements</h4>
-      <div className={styles.endorsementList}>
-        {politician?.endorsements?.organizations.map((endorsement) => (
-          <Link
-            href={`/organizations/${endorsement.slug}`}
-            key={endorsement.id}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div key={endorsement.id} className={styles.endorsementContainer}>
-              <OrganizationAvatar
-                alt={endorsement.name as string}
-                src={endorsement.assets?.thumbnailImage160 as string}
-                size={40}
-              />
-              <span className={styles.endorserLabel}>{endorsement.name}</span>
-            </div>
-          </Link>
-        ))}
-        {politician?.endorsements?.politicians.map((endorsement) => (
-          <Link
-            href={`/politicians/${endorsement.slug}`}
-            key={endorsement.id}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div key={endorsement.id} className={styles.endorsementContainer}>
-              <PartyAvatar
-                alt={endorsement.fullName as string}
-                src={endorsement.assets?.thumbnailImage160 as string}
-                size={40}
-                party={endorsement.party as PoliticalParty}
-                badgeSize="0.75rem"
-                badgeFontSize="0.5rem"
-                theme="light"
-              />
-              <span className={styles.endorserLabel}>
-                {endorsement.fullName}
-              </span>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </section>
-  );
+  const EndorsementSection = () => {
+    if (
+      [
+        ...(politician?.endorsements?.politicians || []),
+        ...(politician?.endorsements?.organizations || []),
+      ].length == 0
+    )
+      return null;
 
-  const $socialSection = (
-    <section className={styles.socials}>
-      {politician?.officialWebsiteUrl && (
-        <a
-          aria-label={"Official Website"}
-          title="Official Website"
-          href={
-            politician.officialWebsiteUrl?.startsWith("http")
-              ? politician.officialWebsiteUrl
-              : `//${politician.officialWebsiteUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GiWireframeGlobe />
-        </a>
-      )}
-      {politician?.campaignWebsiteUrl && (
-        <a
-          aria-label={"Campaign Website"}
-          title="Campaign Website"
-          href={
-            politician.campaignWebsiteUrl?.startsWith("http")
-              ? politician.campaignWebsiteUrl
-              : `//${politician.campaignWebsiteUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGlobe />
-        </a>
-      )}
-      {politician?.twitterUrl && (
-        <a
-          aria-label={"Twitter"}
-          title="Twitter"
-          href={
-            politician.twitterUrl?.startsWith("http")
-              ? politician.twitterUrl
-              : `//${politician.twitterUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTwitter />
-        </a>
-      )}
-      {politician?.facebookUrl && (
-        <a
-          aria-label={"Facebook"}
-          title="Facebook"
-          href={
-            politician.facebookUrl?.startsWith("http")
-              ? politician.facebookUrl
-              : `//${politician.facebookUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaFacebook />
-        </a>
-      )}
-      {politician?.instagramUrl && (
-        <a
-          aria-label={"Instagram"}
-          title="Instagram"
-          href={
-            politician.instagramUrl?.startsWith("http")
-              ? politician.instagramUrl
-              : `//${politician.instagramUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram />
-        </a>
-      )}
-      {politician?.tiktokUrl && (
-        <a
-          aria-label={"TikTok"}
-          title="TikTok"
-          href={
-            politician.tiktokUrl?.startsWith("http")
-              ? politician.tiktokUrl
-              : `//${politician.tiktokUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTiktok />
-        </a>
-      )}
-      {politician?.youtubeUrl && (
-        <a
-          aria-label={"YouTube"}
-          title="YouTube"
-          href={
-            politician.youtubeUrl?.startsWith("http")
-              ? politician.youtubeUrl
-              : `//${politician.youtubeUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaYoutube />
-        </a>
-      )}
-      {politician?.linkedinUrl && (
-        <a
-          aria-label={"LinkedIn"}
-          title="LinkedIn"
-          href={
-            politician.linkedinUrl?.startsWith("http")
-              ? politician.linkedinUrl
-              : `//${politician.linkedinUrl}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-      )}
-    </section>
-  );
+    return (
+      <>
+        <div className={styles.divider} />
+        <section className={styles.endorsementSection}>
+          <h4>Endorsements</h4>
+          <div className={styles.endorsementList}>
+            {politician?.endorsements?.organizations.map((endorsement) => (
+              <Link
+                href={`/organizations/${endorsement.slug}`}
+                key={endorsement.id}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  key={endorsement.id}
+                  className={styles.endorsementContainer}
+                >
+                  <OrganizationAvatar
+                    alt={endorsement.name as string}
+                    src={endorsement.assets?.thumbnailImage160 as string}
+                    size={40}
+                  />
+                  <span className={styles.endorserLabel}>
+                    {endorsement.name}
+                  </span>
+                </div>
+              </Link>
+            ))}
+            {politician?.endorsements?.politicians.map((endorsement) => (
+              <Link
+                href={`/politicians/${endorsement.slug}`}
+                key={endorsement.id}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  key={endorsement.id}
+                  className={styles.endorsementContainer}
+                >
+                  <PartyAvatar
+                    alt={endorsement.fullName as string}
+                    src={endorsement.assets?.thumbnailImage160 as string}
+                    size={40}
+                    party={endorsement.party as PoliticalParty}
+                    badgeSize="0.75rem"
+                    badgeFontSize="0.5rem"
+                    theme="light"
+                  />
+                  <span className={styles.endorserLabel}>
+                    {endorsement.fullName}
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </>
+    );
+  };
+
+  const SocialsSection = () => {
+    if (
+      !politician?.facebookUrl &&
+      !politician?.twitterUrl &&
+      !politician?.tiktokUrl &&
+      !politician?.instagramUrl &&
+      !politician?.youtubeUrl &&
+      !politician?.linkedinUrl &&
+      !politician?.officialWebsiteUrl &&
+      !politician?.campaignWebsiteUrl
+    )
+      return null;
+    return (
+      <>
+        <div className={styles.socialsDivider} />
+        <section className={styles.socials}>
+          {politician?.officialWebsiteUrl && (
+            <a
+              aria-label={"Official Website"}
+              title="Official Website"
+              href={
+                politician.officialWebsiteUrl?.startsWith("http")
+                  ? politician.officialWebsiteUrl
+                  : `//${politician.officialWebsiteUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GiWireframeGlobe />
+            </a>
+          )}
+          {politician?.campaignWebsiteUrl && (
+            <a
+              aria-label={"Campaign Website"}
+              title="Campaign Website"
+              href={
+                politician.campaignWebsiteUrl?.startsWith("http")
+                  ? politician.campaignWebsiteUrl
+                  : `//${politician.campaignWebsiteUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGlobe />
+            </a>
+          )}
+          {politician?.twitterUrl && (
+            <a
+              aria-label={"Twitter"}
+              title="Twitter"
+              href={
+                politician.twitterUrl?.startsWith("http")
+                  ? politician.twitterUrl
+                  : `//${politician.twitterUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+          )}
+          {politician?.facebookUrl && (
+            <a
+              aria-label={"Facebook"}
+              title="Facebook"
+              href={
+                politician.facebookUrl?.startsWith("http")
+                  ? politician.facebookUrl
+                  : `//${politician.facebookUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+          )}
+          {politician?.instagramUrl && (
+            <a
+              aria-label={"Instagram"}
+              title="Instagram"
+              href={
+                politician.instagramUrl?.startsWith("http")
+                  ? politician.instagramUrl
+                  : `//${politician.instagramUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          )}
+          {politician?.tiktokUrl && (
+            <a
+              aria-label={"TikTok"}
+              title="TikTok"
+              href={
+                politician.tiktokUrl?.startsWith("http")
+                  ? politician.tiktokUrl
+                  : `//${politician.tiktokUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok />
+            </a>
+          )}
+          {politician?.youtubeUrl && (
+            <a
+              aria-label={"YouTube"}
+              title="YouTube"
+              href={
+                politician.youtubeUrl?.startsWith("http")
+                  ? politician.youtubeUrl
+                  : `//${politician.youtubeUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </a>
+          )}
+          {politician?.linkedinUrl && (
+            <a
+              aria-label={"LinkedIn"}
+              title="LinkedIn"
+              href={
+                politician.linkedinUrl?.startsWith("http")
+                  ? politician.linkedinUrl
+                  : `//${politician.linkedinUrl}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          )}
+        </section>
+      </>
+    );
+  };
 
   return (
     <article className={styles.widgetContainer}>
@@ -343,24 +380,14 @@ export function PoliticianWidget({
             {$upcomingRaceSection}
           </>
         )}
-        {renderOptions?.endorsements && (
-          <>
-            <div className={styles.divider} />
-            {$endorsementSection}
-          </>
-        )}
+        {renderOptions?.endorsements && <EndorsementSection />}
         {renderOptions?.stats && (
           <>
             <div className={styles.divider} />
             {$statsSection}
           </>
         )}
-        {renderOptions?.socials && (
-          <>
-            <div className={styles.socialsDivider} />
-            {$socialSection}
-          </>
-        )}
+        {renderOptions?.socials && <SocialsSection />}
       </main>
       <WidgetFooter learnMoreHref={`/politicians/${politician?.slug}`} />
     </article>
