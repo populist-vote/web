90f528baab5d61e9ecd730d987bf294045d76ce1
Wiley Conte <wileymckayconte@gmail.com>
2023-11-01 13:49:03 -0400

    Fix auth styles

--- pages/auth/password.tsx
+++ pages/auth/password.tsx
@@ -1,8 +1,9 @@
-import { NextPage } from "next";
 import { ResetPasswordForm } from "components/Auth/PasswordForm";
 import { SupportedLocale } from "types/global";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import nextI18nextConfig from "next-i18next.config";
+import { ReactNode } from "react";
+import { BasicLayout } from "components";
 
 export async function getServerSideProps({
   locale,
@@ -21,8 +22,12 @@ export async function getServerSideProps({
   };
 }
 
-const PasswordPage: NextPage = () => {
-  return <ResetPasswordForm />;
-};
+const PasswordPage = () => <ResetPasswordForm />;
+
+PasswordPage.getLayout = (page: ReactNode) => (
+  <BasicLayout hideFooter hideAuthButtons>
+    {page}
+  </BasicLayout>
+);
 
 export default PasswordPage;
--- pages/auth/reset.tsx
+++ pages/auth/reset.tsx
@@ -1,8 +1,9 @@
-import { NextPage } from "next";
 import { RequestResetForm } from "components/Auth/RequestResetForm";
 import { SupportedLocale } from "types/global";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import nextI18nextConfig from "next-i18next.config";
+import { ReactNode } from "react";
+import { BasicLayout } from "components";
 
 export async function getServerSideProps({
   locale,
@@ -20,8 +21,12 @@ export async function getServerSideProps({
   };
 }
 
-const RequestResetPage: NextPage = () => {
-  return <RequestResetForm />;
-};
+const RequestResetPage = () => <RequestResetForm />;
+
+RequestResetPage.getLayout = (page: ReactNode) => (
+  <BasicLayout hideFooter hideAuthButtons>
+    {page}
+  </BasicLayout>
+);
 
 export default RequestResetPage;
