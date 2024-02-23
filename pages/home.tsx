f26b2debe1e1ee34a6602987031f81c49d4bd1d0
Wiley Conte <wileymckayconte@gmail.com>
2024-02-23 10:38:05 -0700

    Remove beta notice

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,9 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import nextI18nextConfig from "next-i18next.config";
 import { useTranslation } from "next-i18next";
 
-import { BasicLayout, HomePageButton, BetaNotice } from "components";
-import { BETA_NOTICE_VISIBLE } from "utils/constants";
+import { BasicLayout, HomePageButton } from "components";
 import { useAuth } from "hooks/useAuth";
 import { SupportedLocale } from "types/global";
 
@@ -55,26 +54,8 @@ function CitizenHome({ userLoggedIn }: { userLoggedIn: boolean }) {
 
 function HomePage() {
   const { user } = useAuth({ redirect: false });
-  const userId = user?.id;
 
-  const [isBetaVisible, setIsBetaVisible] = useState(
-    localStorage.getItem(`${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`) !==
-      "false"
-  );
-
-  const handleBetaDismissal = () => {
-    setIsBetaVisible(false);
-    localStorage.setItem(
-      `${BETA_NOTICE_VISIBLE}-${userId || "incognito"}`,
-      "false"
-    );
-  };
-
-  return isBetaVisible ? (
-    <BasicLayout>
-      <BetaNotice onContinue={handleBetaDismissal} />
-    </BasicLayout>
-  ) : (
+  return (
     <BasicLayout>
       <CitizenHome userLoggedIn={!!user} />
     </BasicLayout>
--- utils/constants.tsx
+++ utils/constants.tsx
@@ -44,7 +44,6 @@ export const ORGANIZATION_FALLBACK_IMAGE_400_URL =
 export const VOTING_GUIDE_WELCOME_VISIBLE =
   "populist-voting-guide-welcome-visible";
 export const SAVED_GUIDES_LOCAL_STORAGE = "populist-saved-guides-local-storage";
-export const BETA_NOTICE_VISIBLE = "populist-beta-notice-visible";
 export const LAST_SELECTED_EMBED_TYPE = "last-selected-embed-type";
 
 // Session Storage
