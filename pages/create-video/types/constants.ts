bafae90a3eec2f935d0fc3154d4887381826fa3c
Eric Chu <chufucious@gmail.com>
2024-04-16 12:00:06 -0700

    video now 9:16

--- pages/create-video/types/constants.ts
+++ pages/create-video/types/constants.ts
@@ -12,6 +12,6 @@ export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
 };
 
 export const DURATION_IN_FRAMES = 200;
-export const VIDEO_WIDTH = 1280;
-export const VIDEO_HEIGHT = 720;
-export const VIDEO_FPS = 30;
+export const VIDEO_WIDTH = 1080;
+export const VIDEO_HEIGHT = 1920;
+export const VIDEO_FPS = 60;
