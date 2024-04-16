5ffbfcf34f79bc7a58d033e495279f2a2748b045
Eric Chu <chufucious@gmail.com>
2024-04-16 14:50:31 -0700

    slide 1

--- pages/create-video/remotion/MyComp/Main.tsx
+++ pages/create-video/remotion/MyComp/Main.tsx
@@ -35,6 +35,22 @@ const tag: React.CSSProperties = {
   alignItems: "center",
 };
 
+const bigTag: React.CSSProperties = {
+  width: "48rem", // 270pt
+  height: "12rem", // 68pt
+  padding: "1.25rem 1.875rem", // 20pt 30pt
+  borderRadius: "3.75rem",
+  border: "4px solid #FFB45C",
+  backgroundColor: "rgba(255, 180, 92, 0.1)", // 10% opacity
+  fontFamily: "commuterSansLight",
+  fontSize: "4rem",
+  color: "white",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.3)",
+};
+
 export const Main = ({}: z.infer<typeof CompositionProps>) => {
   // const frame = useCurrentFrame();
   const { fps } = useVideoConfig();
@@ -58,46 +74,60 @@ export const Main = ({}: z.infer<typeof CompositionProps>) => {
             display: "flex",
             flexDirection: "column",
             alignItems: "left",
-            justifyContent: "center",
-            padding: "20pt",
+            justifyContent: "top",
+            padding: "8rem 4rem", // 120pt 40pt
+            gap: "4rem",
           }}
         >
-          <h3
-            style={{
-              fontFamily: "commuterSansLight",
-              color: "white",
-              fontSize: "3rem",
-            }}
-          >
-            2023 - 2024 SESSION
-          </h3>
-          <hr
-            style={{ border: "1px solid white", opacity: "0.1", width: "100%" }}
-          ></hr>
-          <h2
-            style={{
-              fontFamily: "commuterSansLight",
-              color: "white",
-              fontSize: "4rem",
-            }}
-          >
-            MN - HF 4746
-          </h2>
-
+          <div id="header" style={{}}>
+            <h3
+              style={{
+                fontFamily: "commuterSansLight",
+                color: "white",
+                fontSize: "2.5rem",
+                margin: "2rem 0",
+              }}
+            >
+              2023 - 2024 SESSION
+            </h3>
+            <hr
+              style={{
+                border: "1px solid white",
+                opacity: "0.1",
+                width: "100%",
+              }}
+            ></hr>
+            <h2
+              style={{
+                fontFamily: "commuterSansLight",
+                color: "white",
+                fontSize: "3rem",
+                margin: "2rem 0",
+              }}
+            >
+              MN - HF 4746
+            </h2>
+          </div>
           <h1
             style={{
               fontFamily: "commuterSansSemiBold",
               color: "white",
               fontSize: "8rem",
+              margin: "30rem 0 0 0",
             }}
           >
             Rideshare Regulations
           </h1>
 
-          <div id="tags" style={{ display: "flex", gap: "8pt" }}>
+          <div
+            id="tags"
+            style={{ display: "flex", gap: "8pt", marginBottom: "2rem" }}
+          >
             <span style={tag}>Tag</span>
             <span style={tag}>Tag</span>
           </div>
+
+          <div style={bigTag}>In Consideration</div>
         </div>
       </Sequence>
     </AbsoluteFill>
