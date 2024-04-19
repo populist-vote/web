88131b99806dc89052a40b4a22fafb98320bdd7f
Eric Chu <chufucious@gmail.com>
2024-04-19 14:28:32 -0700

    added Sponsors data

--- video/legislationVideo/Sponsors.tsx
+++ video/legislationVideo/Sponsors.tsx
@@ -1,65 +1,16 @@
-// import { Sequence } from "remotion";
-// import React from "react";
-// import Image from "next/legacy/image";
-// import billInConsiderationDark from "public/images/video-generator/bill-status-in-consideration-darkbg.svg";
+import { Sequence } from "remotion";
+import React from "react";
+
 // import type { Sponsor } from "generated";
 
-// export const Sponsors = ({ sponsors }: { sponsors: Sponsor[] }) => {
-//   // console.log("Data in Sponsors:", billData);
+export const Sponsors = () => {
+  // console.log("Data in Sponsors:", billData);
+  // partyavatar / candidate / office on candidate politician.office
+  // helper functions in utils/politicans/computeshortofficetitle
 
-//   return (
-//     <Sequence>
-//       <div
-//         style={{
-//           display: "flex",
-//           flexDirection: "column",
-//           alignItems: "left",
-//           justifyContent: "top",
-//           padding: "8rem 4rem",
-//           gap: "4rem",
-//           width: "100%",
-//         }}
-//       >
-//         <h1
-//           style={{
-//             color: "white",
-//             fontSize: "6rem",
-//             fontWeight: "600",
-//             margin: "10rem 0 0 0",
-//           }}
-//         >
-//           Sponsors
-//         </h1>
-//         {sponsors.map((sponsor, index) => (
-//           <div
-//             key={index}
-//             style={{ display: "flex", alignItems: "center", gap: "2rem" }}
-//           >
-//             <Image
-//               src={sponsor.thumbnailImageUrl}
-//               alt={sponsor.fullName}
-//               width={160}
-//               height={160}
-//               style={{ borderRadius: "80px" }}
-//             />
-//             <div>
-//               <h2 style={{ color: "white", fontSize: "3rem" }}>
-//                 {sponsor.fullName}
-//               </h2>
-//               <p style={{ color: "white", fontSize: "2rem" }}>
-//                 {sponsor.party.name}
-//               </p>
-//             </div>
-//           </div>
-//         ))}
-//         <div style={{ position: "absolute", bottom: "20%", width: "400px" }}>
-//           <Image
-//             src={billInConsiderationDark}
-//             alt="Bill Status: In Consideration"
-//             layout="responsive"
-//           />
-//         </div>
-//       </div>
-//     </Sequence>
-//   );
-// };
+  return (
+    <Sequence>
+      <div>sponsors will go here</div>
+    </Sequence>
+  );
+};
--- video/legislationVideo/Video.tsx
+++ video/legislationVideo/Video.tsx
@@ -5,12 +5,68 @@ import { AbsoluteFill, Series } from "remotion";
 // import { Sponsors } from "./Sponsors";
 import { HeaderInner } from "./components/HeaderInner/HeaderInner";
 import type { BillResult } from "generated";
+import Image from "next/legacy/image";
+import billInConsiderationDark from "public/images/video-generator/bill-status-in-consideration-darkbg.svg";
+import { Candidate, ColoredSection } from "components";
 
-export const LegislationVideo = ({ billData }: { billData: BillResult }) => {
+import { PoliticianResult } from "generated";
+
+import styles from "../../pages/bills/BillBySlug.module.scss";
+
+export const LegislationVideo = ({
+  billResult,
+  // sponsors,
+}: {
+  billResult: BillResult;
+}) => {
+  // console.log("LegislationVideo billdata:", JSON.stringify(billData, null, 2));
   return (
     <>
       <AbsoluteFill>
         <Series>
+          <Series.Sequence
+            durationInFrames={Infinity}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "left",
+              justifyContent: "top",
+              padding: "8rem 4rem", // 120pt 40pt
+              gap: "4rem",
+              width: "100%",
+            }}
+          >
+            <HeaderInner
+              billTitle={billResult.populistTitle ?? billResult.title}
+              billNumber={billResult.billNumber}
+              billState={billResult.state}
+              billSession={billResult.session}
+            />
+            {billResult?.sponsors && billResult.sponsors.length > 0 && (
+              <ColoredSection color="var(--blue)">
+                <h2 className={styles.gradientHeader}>Sponsors</h2>
+                <div className={styles.sponsorsWrapper}>
+                  {billResult.sponsors.map((sponsor) => (
+                    <Candidate
+                      key={sponsor.id}
+                      itemId={sponsor.id}
+                      candidate={sponsor as PoliticianResult}
+                    />
+                  ))}
+                </div>
+              </ColoredSection>
+            )}
+            {/* <Sponsors /> */}
+            <div
+              style={{ position: "absolute", bottom: "20%", width: "400px" }}
+            >
+              <Image
+                src={billInConsiderationDark}
+                alt="Bill Status: In Consideration"
+                layout="responsive"
+              />
+            </div>
+          </Series.Sequence>
           {/* <Series.Sequence durationInFrames={200}>
             <Main />
           </Series.Sequence>
@@ -32,14 +88,6 @@ export const LegislationVideo = ({ billData }: { billData: BillResult }) => {
             />
             <LastVotes />
           </Series.Sequence> */}
-          <Series.Sequence durationInFrames={Infinity}>
-            <HeaderInner
-              billTitle={billData.populistTitle ?? billData.title}
-              billNumber={billData.billNumber}
-              billState={billData.state}
-              billSession={billData.session}
-            />
-          </Series.Sequence>
         </Series>
       </AbsoluteFill>
     </>
