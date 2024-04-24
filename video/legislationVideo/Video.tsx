7716004cfc02a4b68e59437df7b196f3bd585678
Eric Chu <chufucious@gmail.com>
2024-04-24 11:44:03 -0700

    refactored code

--- video/legislationVideo/Video.tsx
+++ video/legislationVideo/Video.tsx
@@ -1,128 +1,119 @@
-import React from "react";
+import React, { memo } from "react";
 import { AbsoluteFill, Series } from "remotion";
-
 import { IssueTags, LegislationStatusBox } from "components";
-
 import type { BillResult, IssueTagResult } from "generated";
 import { splitAtDigitAndJoin } from "utils/strings";
-
-import { HeaderInner } from "./components/HeaderInner/HeaderInner";
+import HeaderInner from "./components/HeaderInner/HeaderInner";
 import VoteDisplay from "./components/VoteDisplay";
 import SponsorDisplay from "./components/SponsorDisplay";
 import StatusBadge from "./components/StatusBadge";
 import Logos from "./components/Logos";
 import styles from "./LegislationVideo.module.scss";
 
-export const LegislationVideo = ({
+interface Vote {
+  date: string;
+  chamber: string;
+  yea: number;
+  nay: number;
+}
+const getLastVote = (votes: Vote[], chamber: string): Vote | undefined =>
+  votes
+    ?.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .find((vote) => vote.chamber === chamber);
+
+export const LegislationVideoComponent = ({
   billResult,
 }: {
   billResult: BillResult;
 }) => {
+  const { populistTitle, title, issueTags, legiscanData, sponsors, status } =
+    billResult;
   const headerInnerProps = {
-    title: billResult.populistTitle ?? billResult.title,
+    title: populistTitle ?? title,
     billNumber: billResult.billNumber,
     state: billResult.state,
     session: billResult.session,
   };
-  const lastHouseVote = billResult.legiscanData?.votes
-    ?.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-    .filter((vote) => vote.chamber === "H")
-    .slice(-1)
-    .pop();
 
-  const lastSenateVote = billResult.legiscanData?.votes
-    ?.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-    .filter((vote) => vote.chamber === "S")
-    .slice(-1)
-    .pop();
+  const lastHouseVote = getLastVote(legiscanData?.votes ?? [], "H");
+  const lastSenateVote = getLastVote(legiscanData?.votes ?? [], "S");
 
   return (
-    <>
-      <AbsoluteFill style={{ backgroundColor: "var(--black)" }}>
-        <Series>
-          <Series.Sequence
-            durationInFrames={200}
-            className={styles.legislationVideo}
-          >
-            <div className={styles.mainHeader}>
-              <h3>2023 - 2024 SESSION</h3>
-              <hr></hr>
-              <h2>
-                {billResult.state || "U.S."} -{" "}
-                {splitAtDigitAndJoin(billResult.billNumber)}
-              </h2>
-            </div>
+    <AbsoluteFill style={{ backgroundColor: "var(--black)" }}>
+      <Series>
+        <Series.Sequence
+          durationInFrames={200}
+          className={styles.legislationVideo}
+        >
+          <div className={styles.mainHeader}>
+            <h3>2023 - 2024 SESSION</h3>
+            <hr />
+            <h2>
+              {billResult.state || "U.S."} -{" "}
+              {splitAtDigitAndJoin(billResult.billNumber)}
+            </h2>
+          </div>
+          <div className={styles.bottomContainer}>
+            <h1>{populistTitle || title}</h1>
+            {issueTags && <IssueTags tags={issueTags as IssueTagResult[]} />}
+            <LegislationStatusBox status={status} />
+          </div>
+        </Series.Sequence>
 
-            <div className={styles.bottomContainer}>
-              <h1>{billResult.populistTitle ?? billResult.title}</h1>
-              <div className={styles.issueTags}>
-                {billResult?.issueTags && (
-                  <IssueTags tags={billResult.issueTags as IssueTagResult[]} />
-                )}
-              </div>
-              <div className={styles.statusContainer}>
-                <LegislationStatusBox status={billResult.status} />
-              </div>
-            </div>
-          </Series.Sequence>
-          <Series.Sequence
-            durationInFrames={200}
-            className={styles.legislationVideo}
-          >
-            <HeaderInner headerProps={headerInnerProps} />
-            <p>
-              {billResult.description ??
-                billResult.populistSummary ??
-                billResult.officialSummary}
-            </p>
-            <StatusBadge status={billResult?.status} />
-          </Series.Sequence>
-          <Series.Sequence
-            durationInFrames={200}
-            className={styles.legislationVideo}
-          >
-            <HeaderInner headerProps={headerInnerProps} />
-            <h1>Last Votes</h1>
+        <Series.Sequence
+          durationInFrames={200}
+          className={styles.legislationVideo}
+        >
+          <HeaderInner headerProps={headerInnerProps} />
+          <p>
+            {billResult.description ??
+              billResult.populistSummary ??
+              billResult.officialSummary}
+          </p>
+          <StatusBadge status={status} />
+        </Series.Sequence>
 
-            {lastHouseVote?.yea || lastHouseVote?.nay ? (
-              <VoteDisplay
-                voteTitle={"HOUSE"}
-                numberOfYesVotes={lastHouseVote?.yea ?? 0}
-                numberOfNoVotes={lastHouseVote?.nay ?? 0}
-              />
-            ) : (
-              <></>
-            )}
+        <Series.Sequence
+          durationInFrames={200}
+          className={styles.legislationVideo}
+        >
+          <HeaderInner headerProps={headerInnerProps} />
+          <h1>Last Votes</h1>
+          {lastHouseVote && (
+            <VoteDisplay
+              voteTitle="HOUSE"
+              numberOfYesVotes={lastHouseVote.yea ?? 0}
+              numberOfNoVotes={lastHouseVote.nay ?? 0}
+            />
+          )}
+          {lastSenateVote && (
+            <VoteDisplay
+              voteTitle="SENATE"
+              numberOfYesVotes={lastSenateVote.yea ?? 0}
+              numberOfNoVotes={lastSenateVote.nay ?? 0}
+            />
+          )}
+          <StatusBadge status={status} />
+        </Series.Sequence>
 
-            {lastSenateVote?.yea || lastSenateVote?.nay ? (
-              <VoteDisplay
-                voteTitle={"SENATE"}
-                numberOfYesVotes={lastSenateVote?.yea ?? 0}
-                numberOfNoVotes={lastSenateVote?.nay ?? 0}
-              />
-            ) : (
-              <></>
-            )}
-            <StatusBadge status={billResult?.status} />
-          </Series.Sequence>
-          <Series.Sequence
-            durationInFrames={200}
-            className={styles.legislationVideo}
-          >
-            <HeaderInner headerProps={headerInnerProps} />
+        <Series.Sequence
+          durationInFrames={200}
+          className={styles.legislationVideo}
+        >
+          <HeaderInner headerProps={headerInnerProps} />
+          <SponsorDisplay sponsors={sponsors} />
+          <StatusBadge status={status} />
+        </Series.Sequence>
 
-            <SponsorDisplay sponsors={billResult.sponsors} />
-
-            <StatusBadge status={billResult?.status} />
-          </Series.Sequence>
-          <Series.Sequence
-            durationInFrames={200}
-            className={styles.legislationVideo}
-          >
-            <Logos />
-          </Series.Sequence>
-        </Series>
-      </AbsoluteFill>
-    </>
+        <Series.Sequence
+          durationInFrames={200}
+          className={styles.legislationVideo}
+        >
+          <Logos />
+        </Series.Sequence>
+      </Series>
+    </AbsoluteFill>
   );
 };
+
+export const LegislationVideo = memo(LegislationVideoComponent);
