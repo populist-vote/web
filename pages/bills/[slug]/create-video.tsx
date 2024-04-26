15dbaec5080fe52247318c008c935e48064bfec9
Eric Chu <chufucious@gmail.com>
2024-04-25 22:15:21 -0700

    added debug area for testing

--- pages/bills/[slug]/create-video.tsx
+++ pages/bills/[slug]/create-video.tsx
@@ -4,8 +4,8 @@ import { useRouter } from "next/router";
 import { LegislationVideo } from "../../../video/legislationVideo/Video";
 import { VIDEO_FPS, VIDEO_HEIGHT, VIDEO_WIDTH } from "types/constants";
 import type { BillResult } from "generated";
-
 import { useBillBySlugQuery } from "generated";
+import Link from "next/link";
 
 const container: React.CSSProperties = {
   maxWidth: 768,
@@ -39,6 +39,43 @@ const CreateVideoPage: NextPage = () => {
         and, when you're ready, click "Generate Video". After the video is
         processed, you can download it directly from the provided link.
       </p>
+
+      <div
+        style={{
+          padding: "2rem",
+          backgroundColor: "var(--blue-darkest)",
+          width: "400px",
+          position: "absolute",
+        }}
+      >
+        <h3>Debug area</h3>
+        <ul>
+          <li>
+            <Link
+              href={`/bills/mnhf1002023-2024/create-video`}
+              style={{ color: "#ffffff" }}
+            >
+              no summary, h&s votes, lots of d sponsors
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={`/bills/mnsf18842023-2024/create-video`}
+              style={{ color: "#ffffff" }}
+            >
+              no summary, no votes, 3 dem sponsors
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={`/bills/us-hb2772-2023-2024/create-video`}
+              style={{ color: "#ffffff" }}
+            >
+              2 issue tags, summary, no votes, no sponsors
+            </Link>
+          </li>
+        </ul>
+      </div>
 
       <div style={container}>
         <div>
