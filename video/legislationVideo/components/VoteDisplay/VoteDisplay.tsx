49aece1e234a3e781a2afdf9e2c91fd35e1d3500
Eric Chu <chufucious@gmail.com>
2024-04-25 19:06:12 -0700

    added animations to VoteDisplay

--- video/legislationVideo/components/VoteDisplay/VoteDisplay.tsx
+++ video/legislationVideo/components/VoteDisplay/VoteDisplay.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/legacy/image";
+import { useCurrentFrame, interpolate, Easing } from "remotion";
 import iconYay from "public/images/video-generator/yay-icon.svg";
 import iconNay from "public/images/video-generator/nay-icon.svg";
 import styles from "./VoteDisplay.module.scss";
@@ -13,15 +14,25 @@ const VoteDisplay = ({
   numberOfYesVotes: number;
   numberOfNoVotes: number;
 }) => {
+  const totalTime = 80; // total time in frames
   const voteWidthHeight = 1; // height & width of each square
   const columnGap = 0.5; // gap between columns
   const rowGap = 0.5; // gap between rows
   const noColor = "var(--red)"; // color for no votes
   const yesColor = "var(--green-support)"; // color for yes votes
   const totalVotes = numberOfYesVotes + numberOfNoVotes;
+  const frame = useCurrentFrame();
+  const easingFunction = Easing.bezier(0.22, 1, 0.36, 1); // easeOutQuint
   const squares = new Array(totalVotes).fill(null).map((_, index) => {
     const isGreen = index < numberOfYesVotes;
     const color = isGreen ? yesColor : noColor;
+    const delay = (totalTime / totalVotes) * index;
+    const opacity = interpolate(frame, [delay, delay + 20], [0, 1], {
+      extrapolateLeft: "clamp",
+      extrapolateRight: "clamp",
+      easing: easingFunction,
+    });
+
     return (
       <div
         key={index}
@@ -30,6 +41,7 @@ const VoteDisplay = ({
           height: `${voteWidthHeight}rem`,
           backgroundColor: color,
           borderRadius: "0.25rem",
+          opacity: opacity,
         }}
       />
     );
