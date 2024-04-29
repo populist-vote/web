b809b350e2d258f28ab82ed53773422c5504bcf2
Eric Chu <chufucious@gmail.com>
2024-04-29 14:22:56 -0700

    removed unused constants

--- types/constants.ts
+++ types/constants.ts
@@ -1,18 +1,10 @@
-import { z } from "zod";
-export const COMP_NAME = "MyComp";
-
 export const CompositionProps = z.object({
   title: z.string(),
   billTitle: z.string(), // Add the billTitle property
 });
 
-export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
-  title: "Enter your title here",
-  billTitle: "Enter your bill title here", // Add a default value for billTitle
-};
-
 export const DURATION_IN_FRAMES = 200;
 export const VIDEO_WIDTH = 1080;
 export const VIDEO_HEIGHT = 1920;
 export const VIDEO_FPS = 60;
-export const SCENE_LENGTH_IN_FRAMES = 4 * VIDEO_FPS;
+export const SCENE_LENGTH_IN_FRAMES = 4 * VIDEO_FPS; // 4 seconds
