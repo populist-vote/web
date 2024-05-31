1fe7416d9fbffe1ac708cabd6212a401512d836c
Eric Chu <chufucious@gmail.com>
2024-05-31 10:39:58 -0700

    removed styling from Error

--- components/Video/Error.tsx
+++ components/Video/Error.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 
-const container: React.CSSProperties = {
-  color: "var(--geist-error)",
-  fontFamily: "var(--geist-font)",
-  paddingTop: "var(--geist-half-pad)",
-  paddingBottom: "var(--geist-half-pad)",
-};
-
-const icon: React.CSSProperties = {
-  height: 20,
-  verticalAlign: "text-bottom",
-  marginRight: 6,
-};
-
 export const ErrorComp: React.FC<{
   message: string;
 }> = ({ message }) => {
   return (
-    <div style={container}>
+    <div>
       <svg
         fill="none"
         shapeRendering="geometricPrecision"
@@ -26,7 +13,6 @@ export const ErrorComp: React.FC<{
         strokeLinejoin="round"
         strokeWidth="2"
         viewBox="0 0 24 24"
-        style={icon}
       >
         <circle cx="12" cy="12" r="10" fill="var(--geist-fill)"></circle>
         <path d="M12 8v4" stroke="currentColor"></path>
