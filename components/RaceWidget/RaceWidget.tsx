a9b0441eb0fc04a55ef96346258e11c9394d2ac9
Henry Lai <lai.henry@gmail.com>
2024-07-16 17:15:08 -0600

    race component refactor

--- components/RaceWidget/RaceWidget.tsx
+++ components/RaceWidget/RaceWidget.tsx
@@ -68,7 +68,11 @@ export function RaceWidget({
             )}
           </section>
         )}
-        <Race race={race} itemId={race.id} isEmbedded={true} />
+
+        <div className={styles.raceContainer}>
+          <Race race={race} itemId={race.id} theme="light" isEmbedded={true} />
+        </div>
+
         {race?.voteType === VoteType.RankedChoice && (
           <span className={styles.resultsInfoLabel}>
             Percentages are first choice votes.
