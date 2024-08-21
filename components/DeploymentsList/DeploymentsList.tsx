30d133372821c070920a8f4b7f55974453e19f01
Wiley Conte <wileymckayconte@gmail.com>
2024-08-20 23:51:52 -0500

    Improve recent deployments design

--- components/DeploymentsList/DeploymentsList.tsx
+++ components/DeploymentsList/DeploymentsList.tsx
@@ -33,66 +33,71 @@ export function DeploymentsList({
 
       {deployments?.map((deployment: EnhancedEmbedOriginResult) => (
         <div className={styles.deploymentRow} key={deployment.lastPingAt}>
-          {deployment.name && <h3>{deployment.name}</h3>}
-          {deployment.embedType && (
-            <Badge
-              theme={EmbedTypeColorMap[deployment.embedType]}
-              size="small"
-              variant="solid"
-            >
-              {deployment.embedType?.replace("_", " ")}
-            </Badge>
-          )}
-
-          <div className={styles.flexLeft}>
-            <ImageWithFallback
-              height="25"
-              width="25"
-              alt={"favicon"}
-              src={`https://www.google.com/s2/favicons?domain=${deployment.url}`}
-              fallbackSrc={"/images/favicon.ico"}
-            />
-            <Link
-              href={deployment.url}
-              key={`${deployment.embedId + deployment.url}`}
-              target="_blank"
-              rel="noreferrer"
-              className={clsx(styles.linkParent, styles.clamp1)}
-            >
-              {deployment.url}
-            </Link>
-          </div>
-          {deployment.lastPingAt && (
-            <div className={styles.flexRight}>
-              <RiEyeFill color="var(--salmon)" />
-              <small style={{ fontStyle: "italic" }}>
-                Last view{" "}
-                {getRelativeTimeString(new Date(deployment.lastPingAt))}
-              </small>
+          <div className={styles.flexColumn}>
+            <div className={styles.flexLeft}>
+              {deployment.name && <h3>{deployment.name}</h3>}
+              {deployment.embedType && (
+                <Badge
+                  theme={EmbedTypeColorMap[deployment.embedType]}
+                  size="small"
+                  variant="solid"
+                >
+                  {deployment.embedType?.replace("_", " ")}
+                </Badge>
+              )}
             </div>
-          )}
-          <div className={styles.flexRight}>
-            <Tooltip content="Visit Page">
-              <button
-                className={styles.iconButton}
-                onClick={() => window.open(deployment.url, "_blank")}
+            <div className={styles.flexLeft}>
+              <ImageWithFallback
+                height="25"
+                width="25"
+                alt={"favicon"}
+                src={`https://www.google.com/s2/favicons?domain=${deployment.url}`}
+                fallbackSrc={"/images/favicon.ico"}
+              />
+              <Link
+                href={deployment.url}
+                key={`${deployment.embedId + deployment.url}`}
+                target="_blank"
+                rel="noreferrer"
+                className={clsx(styles.linkParent, styles.clamp1)}
               >
-                <RiExternalLinkLine color="var(--blue-text-light)" />
-              </button>
-            </Tooltip>
-            <Tooltip content="Manage Embed">
-              <button
-                className={styles.iconButton}
-                onClick={async (e) => {
-                  e.stopPropagation();
-                  await router.push(
-                    `/dashboard/${dashboardSlug}/embeds/${kebabCase(deployment.embedType.toLowerCase())}/${deployment.embedId}/manage`
-                  );
-                }}
-              >
-                <RiListSettingsFill color="var(--blue-text-light)" />
-              </button>
-            </Tooltip>
+                {deployment.url}
+              </Link>
+            </div>
+          </div>
+          <div className={styles.flexColumn}>
+            {deployment.lastPingAt && (
+              <div className={styles.flexRight}>
+                <RiEyeFill color="var(--salmon)" />
+                <small style={{ fontStyle: "italic" }}>
+                  Last view{" "}
+                  {getRelativeTimeString(new Date(deployment.lastPingAt))}
+                </small>
+              </div>
+            )}
+            <div className={styles.flexRight}>
+              <Tooltip content="Visit Page">
+                <button
+                  className={styles.iconButton}
+                  onClick={() => window.open(deployment.url, "_blank")}
+                >
+                  <RiExternalLinkLine color="var(--blue-text-light)" />
+                </button>
+              </Tooltip>
+              <Tooltip content="Manage Embed">
+                <button
+                  className={styles.iconButton}
+                  onClick={async (e) => {
+                    e.stopPropagation();
+                    await router.push(
+                      `/dashboard/${dashboardSlug}/embeds/${kebabCase(deployment.embedType.toLowerCase())}/${deployment.embedId}/manage`
+                    );
+                  }}
+                >
+                  <RiListSettingsFill color="var(--blue-text-light)" />
+                </button>
+              </Tooltip>
+            </div>
           </div>
         </div>
       ))}
