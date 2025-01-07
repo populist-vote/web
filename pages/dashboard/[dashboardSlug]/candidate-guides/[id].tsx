2696311aff2d0c24114e198208c70243bfc92c9a
Wiley Conte <wileymckayconte@gmail.com>
2025-01-07 12:33:26 -0700

    Add 2025 to year dropdown

--- pages/dashboard/[dashboardSlug]/candidate-guides/[id].tsx
+++ pages/dashboard/[dashboardSlug]/candidate-guides/[id].tsx
@@ -821,6 +821,7 @@ function RacesSection({
                 backgroundColor={"blue"}
                 value={year as string}
                 options={[
+                  { value: "2025", label: "2025" },
                   { value: "2024", label: "2024" },
                   { value: "2023", label: "2023" },
                   { value: "2022", label: "2022" },
