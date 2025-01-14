751292082f8c1084c85e7b287e7d42d38f82ba09
Wiley Conte <wileymckayconte@gmail.com>
2025-01-14 16:37:25 -0700

    Display voting for statements regardless of author

--- components/ConversationEmbed/ConversationEmbed.tsx
+++ components/ConversationEmbed/ConversationEmbed.tsx
@@ -19,7 +19,6 @@ import { useMemo, useState } from "react";
 import { LeftArrowIcon } from "components/Icons";
 import { Badge, Divider, LoaderFlag, RadioGroup, TextInput } from "components";
 import { PageIndex } from "components/PageIndex/PageIndex";
-import { useAuth } from "hooks/useAuth";
 import { FaCheckCircle, FaCircle, FaMinusCircle } from "react-icons/fa";
 import { BsCircleFill } from "react-icons/bs";
 import { AiFillCloseCircle } from "react-icons/ai";
@@ -504,8 +503,6 @@ function EmbedStatementBox({
   statement: StatementResult;
   handleVote: (statementId: string, vote: ArgumentPosition) => void;
 }) {
-  const { user } = useAuth();
-  const isUserStatementAuthor = statement?.author?.id === user?.id;
   const currentVote = statement?.voteByUserOrSession;
 
   // Memoize the profile picture URL to help with debugging
@@ -525,49 +522,48 @@ function EmbedStatementBox({
     <div className={styles.statementContainer}>
       <div className={styles.statementBox}>
         <p>{statement.content}</p>
-        {!isUserStatementAuthor && (
-          <div className={styles.voteOptions}>
-            <button
-              className={clsx(styles.voteBadge, styles.support, {
-                [styles.selected as string]:
-                  currentVote === ArgumentPosition.Support,
-              })}
-              onClick={() => handleVote(statement.id, ArgumentPosition.Support)}
-            >
-              <span className={styles.iconStack}>
-                <FaCircle size={21} color="white" />
-                <FaCheckCircle size={21} color="var(--green-support)" />
-              </span>
-              <span>Support</span>
-            </button>
-            <button
-              className={clsx(styles.voteBadge, styles.oppose, {
-                [styles.selected as string]:
-                  currentVote === ArgumentPosition.Oppose,
-              })}
-              onClick={() => handleVote(statement.id, ArgumentPosition.Oppose)}
-            >
-              <span className={styles.iconStack}>
-                <BsCircleFill size={21} color="white" />
-                <AiFillCloseCircle size={21} color="var(--red)" />
-              </span>
-              <span>Oppose</span>
-            </button>
-            <button
-              className={clsx(styles.voteBadge, styles.neutral, {
-                [styles.selected as string]:
-                  currentVote === ArgumentPosition.Neutral,
-              })}
-              onClick={() => handleVote(statement.id, ArgumentPosition.Neutral)}
-            >
-              <span className={styles.iconStack}>
-                <BsCircleFill size={21} color="white" />
-                <FaMinusCircle size={21} color="var(--grey)" />
-              </span>
-              <span>Neutral</span>
-            </button>
-          </div>
-        )}
+
+        <div className={styles.voteOptions}>
+          <button
+            className={clsx(styles.voteBadge, styles.support, {
+              [styles.selected as string]:
+                currentVote === ArgumentPosition.Support,
+            })}
+            onClick={() => handleVote(statement.id, ArgumentPosition.Support)}
+          >
+            <span className={styles.iconStack}>
+              <FaCircle size={21} color="white" />
+              <FaCheckCircle size={21} color="var(--green-support)" />
+            </span>
+            <span>Support</span>
+          </button>
+          <button
+            className={clsx(styles.voteBadge, styles.oppose, {
+              [styles.selected as string]:
+                currentVote === ArgumentPosition.Oppose,
+            })}
+            onClick={() => handleVote(statement.id, ArgumentPosition.Oppose)}
+          >
+            <span className={styles.iconStack}>
+              <BsCircleFill size={21} color="white" />
+              <AiFillCloseCircle size={21} color="var(--red)" />
+            </span>
+            <span>Oppose</span>
+          </button>
+          <button
+            className={clsx(styles.voteBadge, styles.neutral, {
+              [styles.selected as string]:
+                currentVote === ArgumentPosition.Neutral,
+            })}
+            onClick={() => handleVote(statement.id, ArgumentPosition.Neutral)}
+          >
+            <span className={styles.iconStack}>
+              <BsCircleFill size={21} color="white" />
+              <FaMinusCircle size={21} color="var(--grey)" />
+            </span>
+            <span>Neutral</span>
+          </button>
+        </div>
       </div>
       <div className={styles.authorInfo}>
         <div className={styles.flexLeft}>
