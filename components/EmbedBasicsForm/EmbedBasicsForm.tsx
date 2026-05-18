27314fa3434bce19b8ed4e8860c4f630e8ae7300
Henry Lai <lai.henry@gmail.com>
2026-05-18 16:23:25 -0600

    allow linking copy fix 2

--- components/EmbedBasicsForm/EmbedBasicsForm.tsx
+++ components/EmbedBasicsForm/EmbedBasicsForm.tsx
@@ -93,7 +93,7 @@ function RaceEmbedOptionsForm({
         <Checkbox
           id="hideHostPageFromMyBallotMoreInfo"
           name="renderOptions.hideHostPageFromMyBallotMoreInfo"
-          label="Don't allow integrated linking in My Ballot embeds"
+          label="Block integrated linking in My Ballot embeds"
           register={register}
         />
       </div>
